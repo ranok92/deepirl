1191d4a5c81d214bdee431e2e2320c841598732a
Bobak Baghi <bobak.baghi@gmail.com>
2019-02-11 20:46:57 -0500

    Slight reformatting of comments, removed variable redefinition issues.

--- networks.py
+++ networks.py
@@ -7,31 +7,36 @@ This file has the two networks
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
 
-#cost network
+# cost network
+
+
 class CostNetwork(nn.Module):
+    """
+    Cost (reward) network for RL function approx. methods.
+    """
 
-    def __init__(self , nn_params):
+    def __init__(self, _nn_params):
+        """__init__
 
-        # nn_params -  a dictionary containing 3 keys : 'input' - size of input layer , 'hidden' - a list containing size of hidden layers
-        #                                               'output' - size of output layer *for policy the size of the action space
+        :param nn_params: a dictionary containing 3 keys : 'input' - size of
+        input layer , 'hidden' - a list containing size of hidden layers
+        'output' - size of output layer *for policy the size of the action
+        snn_paramspace.
+        """
         super(CostNetwork, self).__init__()
 
-        self.input = nn_params['input']
-        self.output = nn_params['output']
-
-        self.hidden = nn_params['hidden']
+        self.input = _nn_params['input']
+        self.output = _nn_params['output']
+        self.hidden = _nn_params['hidden']
         self.no_of_hidden_layers = len(self.hidden)
-
 
         self.inputLayer = nn.Linear(self.input, self.hidden[0])
         self.hidden1 = nn.Linear(self.hidden[0], self.hidden[1])
-        self.outputLayer = nn.Linear(self.hidden[self.no_of_hidden_layers-1], 1)
+        self.outputLayer = nn.Linear(
+            self.hidden[self.no_of_hidden_layers-1], 1)
 
-
-    def forward(self,x):
-
+    def forward(self, x):
 
         #x - tensor
         x = F.elu(self.inputLayer(x))
@@ -45,17 +50,18 @@ class CostNetwork(nn.Module):
         return x
 
 
-
 class Policy(nn.Module):
+    """
+    Policy network for policy gradient RL methods.
+    """
 
-    def __init__(self, nn_params):
+    def __init__(self, _nn_params):
 
         super(Policy, self).__init__()
-        self.input = nn_params['input']
-        self.output = nn_params['output']
-        self.hidden = nn_params['hidden']
+        self.input = _nn_params['input']
+        self.output = _nn_params['output']
+        self.hidden = _nn_params['hidden']
         self.no_of_hidden_layers = len(self.hidden)
-
 
         self.inputLayer = nn.Linear(self.input, self.hidden[0])
         self.hidden1 = nn.Linear(self.hidden[0], self.hidden[1])
@@ -65,7 +71,6 @@ class Policy(nn.Module):
 
         self.saved_actions = []
         self.rewards = []
-
 
     def forward(self, x):
 
@@ -79,17 +84,16 @@ class Policy(nn.Module):
         return F.softmax(action_scores, dim=-1), state_values
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
 
-
-    nn_params = {'input': 3 , 'hidden':[10] , 'output':1}
+    nn_params = {'input': 3, 'hidden': [10], 'output': 1}
     cNN = CostNetwork(nn_params)
-    inp = torch.rand(2,3)
+    inp = torch.rand(2, 3)
     y = cNN(inp)
     for i in range(2):
-        print 'For iteration ',i
+        print 'For iteration ', i
         y[i].backward(retain_graph=True)
         for p in cNN.parameters():
 
-            print 'The p',p
-            print 'The corresponding grad ',p.grad
+            print 'The p', p
+            print 'The corresponding grad ', p.grad
