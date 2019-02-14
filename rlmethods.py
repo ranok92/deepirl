22b01944fa4de2d74e9a00d5a025c25877a684db
Bobak Baghi <bobak.baghi@gmail.com>
2019-02-13 23:35:48 -0500

    added a new file utils.py. This file should contain utilities that many different files and method might use. If it gets too cluttered, it can be split based on purpose (rl_utils, irl_utils, gym_utils, etc.)

--- rlmethods.py
+++ rlmethods.py
@@ -16,6 +16,7 @@ from torch.distributions import Categorical
 
 from networks import CostNetwork, Policy
 
+from utils import HistoryBuffer
 
 def getMemoryAllocationInfo(memoryInBytes):
     """Converts bytes into GB/MB/KB/B string.
@@ -37,37 +38,6 @@ def getMemoryAllocationInfo(memoryInBytes):
         infoList[3], infoList[2], infoList[1], infoList[0])
 
     return result, infoList
-
-
-class HistoryBuffer:
-
-    def __init__(self, bufferSize=10):
-        self.bufferSize = bufferSize
-        self.buffer = []
-        self.device = torch.device(
-            "cuda" if torch.cuda.is_available() else "cpu")
-
-    def addState(self, state):
-        """ add a state to the history buffer each state is assumed to be of
-        shape ( 1 x S ) """
-
-        if len(self.buffer) >= self.bufferSize:
-            del self.buffer[0]  # remove the oldest state
-
-        self.buffer.append(state.cpu().numpy())
-
-    # returns the 10 states in the buffer in the form of a torch tensor in the
-    # order in which they were encountered
-    def getHistory(self):
-
-        arrSize = self.buffer[0].shape[1]
-        arrayHist = np.asarray(self.buffer)
-
-        arrayHist = np.reshape(arrayHist, (1, arrSize*self.bufferSize))
-        state = torch.from_numpy(arrayHist).to(self.device)
-        state = state.type(torch.cuda.FloatTensor)
-
-        return state
 
 
 class ActorCritic:
