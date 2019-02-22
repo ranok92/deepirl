72d10a28829c6fda496735d7fcbfed54f9847f96
Bobak Baghi <bobak.baghi@gmail.com>
2019-02-21 21:34:14 -0500

    made frozenlake non-slippery

--- gym_envs/__init__.py
+++ gym_envs/__init__.py
@@ -6,7 +6,7 @@ from gym.envs.registration import register
 
 register(id='FrozenLakeNP-v0',
          entry_point='gym_envs.np_frozenlake:FrozenLakeNP',
-         kwargs={'map_name': '4x4'},
+         kwargs={'map_name': '4x4', 'is_slippery':False},
          max_episode_steps=100,
          reward_threshold=0.78,  # optimum = .8196
-         )
+        )
