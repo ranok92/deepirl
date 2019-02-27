Fixed bugs in the state visitation frequency calculation. It works now.

irlmethods/irlUtils.py
@@ -11,9 +11,14 @@ import math
 from rlmethods.b_actor_critic import ActorCritic
 from rlmethods.b_actor_critic import Policy
 
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#for visual
+import matplotlib.pyplot as plt
 
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 dtype = torch.float32
+
+
 def createStateActionTable(policy , rows= 10 , cols=10 , num_actions = 4):
 	'''
 	given a particular policy and info about the environment on which it is trained
@@ -40,9 +45,10 @@ def createStateActionTable(policy , rows= 10 , cols=10 , num_actions = 4):
 def createStateTransitionMatix(rows = 10 , cols = 10 , action_space = 5):
 
 	'''
-	Creates a matrix of dimension (total_states X total_states X total_actions)
+	Creates a matrix of dimension (total_states X total_actionsX total_states)
 	As the environment is deterministic all the entries here should be either 
 	0 or 1
+	TranstionMatirx[next_state , action , prev_state]
 	'''
 	total_states = rows*cols
 	transitionMatrix = np.zeros([total_states,action_space,total_states])
@@ -51,22 +57,22 @@ def createStateTransitionMatix(rows = 10 , cols = 10 , action_space = 5):
 		#check for boundary cases before making changes
 		#check left if true then not boundary case
 		if int(i/cols)==int((i-1)/cols):
-			transitionMatrix[i,3,i-1] = 1
+			transitionMatrix[i-1,3,i] = 1
 		else:
 			transitionMatrix[i,3,i] = 1
 		#check right
 		if (int(i/cols)==int((i+1)/cols)):
-			transitionMatrix[i,1,i+1] = 1
+			transitionMatrix[i+1,1,i] = 1
 		else:
 			transitionMatrix[i,1,i] = 1
 		#check top
 		if (math.floor((i-cols)/cols) >= 0):
-			transitionMatrix[i,0,i-cols] = 1
+			transitionMatrix[i-cols,0,i] = 1
 		else:
 			transitionMatrix[i,0,i] = 1
 		#check down
 		if (math.floor((i+cols)/cols)) < cols:
-			transitionMatrix[i,2,i+cols] = 1
+			transitionMatrix[i+cols,2,i] = 1
 		else:
 			transitionMatrix[i,2,i] = 1
 
@@ -100,7 +106,7 @@ def getStateVisitationFreq(policyfile , rows = 10 , cols = 10 , num_actions = 5)
 		total_states x 1
 	'''
 
-	TIMESTEPS = 100
+	TIMESTEPS = 5
 	TOTALSTATES = rows*cols
 	stateVisitationMatrix = np.zeros([TOTALSTATES,TIMESTEPS])
 	env = GridWorld(display=False, obstacles=[np.asarray([1, 2])])
@@ -130,15 +136,22 @@ def getStateVisitationFreq(policyfile , rows = 10 , cols = 10 , num_actions = 5)
 						stateVisitationMatrix[s,i] += stateVisitationMatrix[s_prev,i-1]* \
 											stateTransitionMatrix[s,a,s_prev]*stateActionTable[a,s_prev]
 
-
+	print("summing over time")
+	print(np.sum(stateVisitationMatrix,axis = 0))
 	return np.sum(stateVisitationMatrix,axis=1)
 
 if __name__=='__main__':
 
-	statevisit = getStateVisitationFreq("/home/abhisek/Study/Robotics/deepirl/experiments/saved-models/0.pt" , rows = 10, cols = 10, num_actions = 5)	
+	r = 10
+	c = 10
+	statevisit = getStateVisitationFreq("/home/abhisek/Study/Robotics/deepirl/experiments/saved-models/1.pt" , rows = r, cols = c, num_actions = 5)
 	print(type(statevisit))
 	print(statevisit)
+	statevisitMat = np.resize(statevisit,(r,c))
+	statevisitMat/=5
+	plt.imshow(statevisitMat)
+	plt.colorbar()
+	plt.show()
 	#print(stateactiontable)
 	#print(np.sum(stateactiontable,axis=0))
 	#mat = createStateTransitionMatix(rows=5,cols=5)
-	