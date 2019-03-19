bb2dbf54a4a73ff2908fe7a257e33fa9a6ac072d
Abhisek Konar <konarabhisek@gmail.com>
2019-03-19 09:17:30 -0400

    Ignore the message for the commit d6e2893d870aac956f0fd3636928cba94492c7a4. The two new feature extractors are added in this commit.

--- featureExtractor/gridworld_featureExtractor.py
+++ featureExtractor/gridworld_featureExtractor.py
@@ -4,6 +4,17 @@ specifically for the 10x10 super simplified gridworld environment
 '''
 import pdb
 import numpy as np 
+from utils import reset_wrapper, step_wrapper
+
+
+#*************array of helper methods***************#
+
+#helper methods
+def block_to_arrpos(self,r,c):
+	a = (self.window_size**2-1)/2
+	b = self.window_size
+	pos = a+(b*r)+c
+	return int(pos)
 
 
 class LocalGlobal():
@@ -63,19 +74,143 @@ class LocalGlobal():
 		            temp_pos = np.asarray([agent_pos[0] + r*block_width, 
 		            			agent_pos[1] + c*block_width])
 		            if np.array_equal(temp_pos,obs_pos):
-		                pos = self.block_to_arrpos(r,c)
+		                pos = block_to_arrpos(r,c)
 
 		                mod_state[pos+4]=1
 
-		return mod_state
+		return reset_wrapper(mod_state)
 
 
-	#helper methods
-	def block_to_arrpos(self,r,c):
-		a = (self.window_size**2-1)/2
-		b = self.window_size
-		pos = a+(b*r)+c
-		return int(pos)
+
+class FrontBackSide():
+
+	def __init__(self,window_size=5,grid_size=1):
+
+		#heading direction 0 default top, 1 right ,2 down and 3 left
+		#self.heading_direction=0 #no need for previous heading as the
+		#coordinates provided as the state are always assumed as top facing
+		self.window_size = window_size
+		self.sensor_rad = (int)(window_size/2)
+		self.grid_size = grid_size
+		#the entire table is not needed, only the first row
+		#but I am still keeping this if necessary in future
+		'''
+		format of matrix for coordinate conversion from a direction
+		to the other
+			to 
+		from		top | left | right |down
+			top
+			left
+			right
+			down
+
+
+		'''
+
+
+		self.rel_pos_transform_table = np.asarray([
+												[[1,1],[-1,1],[1,-1],[-1,-1]],
+												[[-1,1],[1,1],[-1,-1],[1,-1]],
+												[[1,-1],[-1,-1],[1,1],[-1,1]],
+												[[-1,-1],[1,-1],[-1,1],[1,1]]
+												])
+
+
+
+	#given the current state returns the relative position of 
+	#the goal and all of the obstacles
+	def get_relative_coords(self,state):
+
+		rel_positions = np.zeros(state.shape[0],state.shape[1])
+		agent_pos = state[0]
+		for i in range(state.shape[0]):
+			rel_position[i,:] = state[i,:]-agent_pos
+
+		return rel_positions
+
+	#update the relative coordinates based on the current heading
+	#and the action taken
+	def update_relative_coords(self, rel_positions,action):
+
+		#using the action and the current heading, decide the final heading
+		#quick reminder 0 - move front 1 - move right 2 - move down 3 - move left
+		multiplying_factor = self.rel_pos_transform_table[0,action]
+
+		for i in range(rel_positions.shape[0]):
+
+			rel_positions[i,0] = rel_positions[i,0]*multiplying_factor[0]
+			rel_positions[i,1] = rel_positions[i,1]*multiplying_factor[1]
+
+		return rel_positions
+
+
+	#given the correct local_representation returns the state representation
+	def rel_coord_to_local_rep(self,rel_pos):
+		'''
+		convention for local representation same as always:
+		0 : front, 1 : right ,2 : back , 3 : left 
+		'''
+		goal_pos = np.zeros(4)
+		local_rep = np.zeros(4)
+		window_size = self.window_size
+		block_width = self.grid_size
+		window_rows = window_cols = window_size
+		row_start =  int((window_rows-1)/2)
+		col_start = int((window_cols-1)/2)
+		#check if they are within the window range and if so do your magic
+		for i in range(1,rel_pos.shape[0]):
+			#if i==1, this means we are dealing with the goal rather than
+			#the obstacles so, that gets added in the goal_pos array
+			#rather than the local_rep array, which is just for the obstacles
+			if np.all(rel_pos[i,:]>=-2) and np.all(rel_pos[i,:]<=2):
+
+				#this value is within range, place it in the right position
+				r = rel_pos[i,0]
+				c = rel_pos[i,1]
+				if abs(r)>abs(c):
+					#front or back
+					if r>0:
+						#back
+						if i==1:
+							goal_pos[2]=1
+						else:
+							local_rep[2]=1
+
+					else:
+						#front
+						if i==1:
+							goal_pos[0]=1
+						else:
+							local_rep[0]=1
+				if abs(r)<=abs(c):
+					#left or right
+					if c<0:
+						#left
+						if i==1:
+							goal_pos[3]=1
+						else:
+							local_rep[3]=1
+					if c>0:
+						#right
+						if i==1:
+							goal_pos[1]=1
+						else:
+							local_rep[1]=1
+
+		return np.concat(goal_pos,local_rep,axis=0)
+
+
+	def extract_features(self,state,action):
+
+		rel_coords = get_relative_coords(state)
+		updated_coords = update_relative_coords(rel_coords,action)
+		features = rel_coord_to_local_rep(updated_coords)
+
+		return reset_wrapper(features)
+
+
+
+
 
 
 
