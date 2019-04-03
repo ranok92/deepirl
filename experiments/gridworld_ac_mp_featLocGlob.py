d4fb7d98a6cbe5620f8be04696bc97492809a1a1
Abhisek Konar <konarabhisek@gmail.com>
2019-04-03 17:19:43 -0400

    Added feature for user to play and record trajectory. Also added feature for training custom policy networks on any provided reward network.

--- experiments/gridworld_ac_mp_featLocGlob.py
+++ experiments/gridworld_ac_mp_featLocGlob.py
@@ -5,41 +5,73 @@ sys.path.insert(0, '..')  # NOQA: E402
 import numpy as np
 import argparse
 import torch.multiprocessing as mp
-from envs.gridworld_clockless import GridWorldClockless as GridWorld
 from rlmethods.b_actor_critic import ActorCritic
 
-from featureExtractor.gridworld_featureExtractor import FrontBackSide
+from featureExtractor.gridworld_featureExtractor import FrontBackSide,LocalGlobal,OneHot,SocialNav,FrontBackSideSimple
 
 from utils import step_wrapper, reset_wrapper
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--policy-path', type=str, nargs='?', default=None)
+parser.add_argument('--reward-path' , type=str, nargs='?', default= None)
 parser.add_argument('--play', action='store_true',
                     help='play given or latest stored policy.')
+parser.add_argument('--play-user' , action='store_true' , 
+                    help='lets the user play the game')
 parser.add_argument('--dont-save', action='store_true',
                    help="don't save the policy network weights.")
 parser.add_argument('--render', action='store_true', help="show the env.")
 parser.add_argument('--num-trajs', type=int, default=10)
+parser.add_argument('--view-reward', action='store_true')
 
 def main():
     
     args = parser.parse_args()
     mp.set_start_method('spawn')
 
-    featExtract = FrontBackSide(fieldList = ['agent_state','goal_state','obstacles'])
+    if args.render:
+        from envs.gridworld import GridWorld
+    else:
+        from envs.gridworld_clockless import GridWorldClockless as GridWorld
 
-    env = GridWorld(display=args.render, is_onehot= False, obstacles=[np.asarray([1, 2])])
+    #featExtract = LocalGlobal(window_size=3, fieldList = ['agent_state','goal_state','obstacles'])
+    #featExtract = OneHot(grid_rows=10,grid_cols=10)
+    featExtract = FrontBackSideSimple(thresh1 = 1,fieldList =  ['agent_state','goal_state','obstacles'])
 
-    model = ActorCritic(env, feat_extractor=featExtract,  gamma=0.99, log_interval=100, max_episodes=10**4,
-                        max_ep_length=30)
+    #featExtract = SocialNav(fieldList = ['agent_state','goal_state'])
+    '''
+    np.asarray([2,2]),np.asarray([7,4]),np.asarray([3,5]),
+                                np.asarray([5,2]),np.asarray([8,3]),np.asarray([7,5]),
+                                np.asarray([3,3]),np.asarray([3,7]),np.asarray([5,7])
+                                '''
+    env = GridWorld(display=args.render, is_onehot= False,is_random=True,
+                    rows =10,
+                    cols =10,
+                    seed = 7,
+                    obstacles = [np.asarray([2,2]),np.asarray([7,4]),np.asarray([3,5]),
+                                np.asarray([5,2]),np.asarray([8,3]),np.asarray([7,5]),],
+                                
+                    goal_state = np.asarray([5,5]))
+
+    model = ActorCritic(env, feat_extractor=featExtract,  gamma=0.99,
+                        log_interval=400,max_ep_length=20 , 
+                        max_episodes = 8000)
 
     if args.policy_path is not None:
         model.policy.load(args.policy_path)
+        
+    if not args.play and not args.play_user:
+        if args.reward_path is None:
+            model.train_mp(n_jobs=4)
+        else:
+            from irlmethods.deep_maxent import RewardNet
+            state_size = featExtract.extract_features(env.reset()).shape[0]
+            reward_net = RewardNet(state_size)
+            reward_net.load(args.reward_path)
+            print(next(reward_net.parameters()).is_cuda)
+            model.train_mp(reward_net = reward_net,n_jobs = 4)
 
-    if not args.play:
-        model.train_mp(n_jobs=4)
-
-        if not args.dont_save:
+        if not args.dont_save:  
             model.policy.save('./saved-models/')
 
     if args.play:
@@ -47,6 +79,11 @@ def main():
         assert args.policy_path is not None, 'pass a policy to play from!'
 
         model.generate_trajectory(args.num_trajs, './trajs/ac_gridworld/')
+
+    if args.play_user:
+        env.tickSpeed = 200
+
+        model.generate_trajectory_user(args.num_trajs, './trajs/ac_gridworld_user/')
 
 if __name__ == '__main__':
     main()
