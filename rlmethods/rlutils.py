2b2eed3b3f045f3ad627852abf6baa0bf1f274f5
Bobak Baghi <bobak.baghi@gmail.com>
2019-07-24 19:35:50 -0400

    Cleanup and autopep8

--- rlmethods/rlutils.py
+++ rlmethods/rlutils.py
@@ -1,24 +1,23 @@
-'''
-file should contain methods and classes needed specifically to help run the rl methods
-'''
-import numpy as np 
-import pdb
-from matplotlib import pyplot as plt
+'''General RL utilities.'''
 import sys
-sys.path.insert(0, '..')
+import pdb
 import random
 import collections
 
-class LossBasedTermination():
+from matplotlib import pyplot as plt
+import numpy as np
+sys.path.insert(0, '..')
 
+
+class LossBasedTermination():
 
     def __init__(
             self,
-            list_size = 100,
-            stop_threshold = .5,
+            list_size=100,
+            stop_threshold=.5,
             info=True,
-            log_interval = 50
-            ):
+            log_interval=50
+    ):
 
         self.loss_diff_list = []
         self.list_size = list_size
@@ -38,50 +37,50 @@ class LossBasedTermination():
 
         elif len(self.loss_diff_list) == self.list_size:
 
-            new_diff = abs(self.last_loss-new_loss)
-            del(self.loss_diff_list[0])#remove the oldest loss entry
+            new_diff = abs(self.last_loss - new_loss)
+            del(self.loss_diff_list[0])  # remove the oldest loss entry
             self.loss_diff_list.append(new_diff)
             self.last_loss = new_loss
-            self.current_avg_loss = sum(self.loss_diff_list)/self.list_size
+            self.current_avg_loss = sum(self.loss_diff_list) / self.list_size
 
         else:
 
-            new_diff = abs(self.last_loss-new_loss)
+            new_diff = abs(self.last_loss - new_loss)
             self.loss_diff_list.append(new_diff)
             self.last_loss = new_loss
 
-            if len(self.loss_diff_list)==self.list_size:
-                self.current_avg_loss = sum(self.loss_diff_list)/self.list_size
+            if len(self.loss_diff_list) == self.list_size:
+                self.current_avg_loss = sum(
+                    self.loss_diff_list) / self.list_size
 
         if self.current_avg_loss is not None:
             if self.info:
                 self.current_avg_loss_diff_list.append(self.current_avg_loss)
 
-
     def check_termination(self):
 
-        if len(self.loss_diff_list)==self.list_size:
+        if len(self.loss_diff_list) == self.list_size:
 
             if self.current_avg_loss < self.stop_threshold:
 
                 return True
 
-
         return False
-
 
     def plot_avg_loss(self):
 
         if self.info:
-            if len(self.current_avg_loss_diff_list) >0 and len(self.current_avg_loss_diff_list)%self.log_interval==0:
+            if len(self.current_avg_loss_diff_list) > 0 and len(self.current_avg_loss_diff_list) % self.log_interval == 0:
                 plt.plot(self.current_avg_loss_diff_list)
                 plt.draw()
                 plt.pause(.0001)
         else:
             pass
 
+
 class ReplayBuffer():
     """A general replay buffer for reinforcement learning replay."""
+
     def __init__(self, max_length):
         self.buffer = collections.deque(maxlen=max_length)
 
@@ -100,14 +99,13 @@ class ReplayBuffer():
         return random.sample(self.buffer, n)
 
 
-
 if __name__ == '__main__':
 
-    l = LossBasedTermination(list_size=10,stop_threshold=3,log_interval=100)
+    l = LossBasedTermination(list_size=10, stop_threshold=3, log_interval=100)
     losslist = [random.random() for _ in range(10000)]
     for val in losslist:
         l.update_list(val)
-        print('avg loss :',l.current_avg_loss)
+        print('avg loss :', l.current_avg_loss)
         print(l.loss_diff_list)
         print(l.check_termination())
         l.plot_avg_loss()
