0ae4abfe3e4ea78d8b01355066e46aa258f3f49a
MacBobak <bobak.baghi@gmail.com>
2019-08-13 16:28:48 -0400

    Added a basic testing script to test soft_ac training.

---
+++ experiments/simple_gw_soft_ac.py
@@ -0,0 +1,42 @@
+import sys
+import numpy as np
+import torch
+sys.path.insert(0, '..')  # NOQA: E402
+
+from rlmethods.soft_ac import SoftActorCritic
+from envs.simple_gw import SimpleGridworld
+
+
+def play(rl, gw):
+    done = False
+
+    state = torch.from_numpy(gw.reset())
+    total_reward = 0
+    iters = 0
+    while not done and iters < 30:
+        action, _ = rl.select_action(state)
+        next_state, reward, done, _ = gw.step(action)
+        total_reward += reward
+
+        iters += 1
+
+    return total_reward
+
+
+def main():
+    env = SimpleGridworld((10, 10), np.array([5, 5]), np.array([7, 7]))
+    soft_ac = SoftActorCritic(env, replay_buffer_size=10**4)
+
+    for i in range(10000):
+        soft_ac.train()
+
+        if i % 100 == 0:
+            rs = []
+            for j in range(10):
+                rs.append(play(soft_ac, env))
+
+            print(np.mean(rs))
+
+
+if __name__ == "__main__":
+    main()
