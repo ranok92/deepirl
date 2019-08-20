Added commandline argument for clipping the gradient. Added more information to plot including the true reward and the reward as dictated by the reward network in each iteration. Fixed some minor issues in the calculation of the SVF. Added methods to view memory usage.

irlmethods/deep_maxent.py
@@ -25,11 +25,10 @@ import utils  # NOQA: E402
 from irlmethods import irlUtils
 # from irlmethods.irlUtils import getStateVisitationFreq  # NOQA: E402
 from neural_nets.base_network import BaseNN
-
+from torch.nn.utils import clip_grad_norm_
 
 from torch.optim.lr_scheduler import StepLR
 from rlmethods.b_actor_critic import Policy
-
 
 class RewardNet(BaseNN):
     """Reward network"""
@@ -89,7 +88,8 @@ class DeepMaxEnt():
             regularizer=0.1,
             learning_rate=1e-3,
             scale_svf=True,
-            seed=10 
+            seed=10, 
+            clipping_value=None
     ):
 
         # pass the actual object of the class of RL method of your choice
@@ -97,7 +97,7 @@ class DeepMaxEnt():
         self.env = env
         self.max_episodes = iterations
         self.traj_path = traj_path
-        self.rl_max_episodes = rl_max_episodes
+        self.rl_max_episodes = self.rl.max_ep_length
         self.graft = graft
 
     # TODO: These functions are replaced in the rl method already, this
@@ -147,6 +147,9 @@ class DeepMaxEnt():
         else:
             print(self.plot_save_folder)
             os.mkdir(self.plot_save_folder)
+
+
+        self.clipping = clipping_value
 
     #******parts being operated on
     def expert_svf(self):
@@ -215,19 +218,35 @@ class DeepMaxEnt():
 
 
     def calculate_grads(self, optimizer, stateRewards, freq_diff):
+
         optimizer.zero_grad()
         dot_prod = torch.dot(stateRewards.squeeze(), freq_diff.squeeze())
-        
+
+
         #adding L1 regularization
         lambda1 = self.regularizer
         l1_reg = torch.tensor(0,dtype=torch.float).to(self.device)
+        grad_mag = torch.tensor(0, dtype=torch.float).to(self.device)
+
         for param in self.reward.parameters():
             l1_reg += torch.norm(param,1)
 
-        loss = dot_prod+(lambda1*l1_reg)   
+        loss = dot_prod+(lambda1*l1_reg) 
         loss.backward()
-        self.lr_scheduler.step()
-        return loss, dot_prod , lambda1*l1_reg, torch.norm(stateRewards.squeeze(), 1)
+
+        #clipping if asked for
+        if self.clipping is not None:
+            clip_grad_norm_(self.reward.parameters(), self.clipping)
+
+
+        for param in self.reward.parameters():
+            grad_mag +=torch.norm(param.grad, 1)
+
+        #print ('The magnitude of gradients after clipping:', grad_mag)
+        #print('The magnitude of the parameters :', l1_reg)
+
+
+        return loss, dot_prod , l1_reg, grad_mag, torch.norm(stateRewards.squeeze(), 1)
 
 
     '''
@@ -305,7 +324,7 @@ class DeepMaxEnt():
     def plot_info(self,inp_tuple):
         #pass a tuple containing n number of lists , this function goes through all and plots them
         i = 0
-        color_list  = ['r','g','b','c','m']
+        color_list  = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'r']
         for list_val in inp_tuple:
             plt.figure(i)
             plt.plot(list_val,color_list[i])
@@ -478,6 +497,11 @@ class DeepMaxEnt():
         l1_reg_list = []
         rewards_norm_list = []
 
+        #added new
+        model_performance_list = [] #list to store the raw score obtained by the current policy
+        model_performance_nn = []
+        reward_grad_norm_list = []
+
         for i in range(self.max_episodes):
             print('starting iteration %s ...'% str(i))
 
@@ -501,14 +525,20 @@ class DeepMaxEnt():
             print('Completed RL training.')
             #np.random.seed(11)
             print('Starting sampling agent-svf. . .')
-            current_agent_svf = self.agent_svf_sampling_dict(num_of_samples=3000,
+            current_agent_svf, true_reward, nn_reward = self.agent_svf_sampling_dict(num_of_samples=500,
                                                              env=self.env,
                                                              policy_nn=self.rl.policy,
                                                              reward_nn=self.reward,
                                                              feature_extractor=self.rl.feature_extractor,
                                                              episode_length=self.rl_max_episodes,
                                                              smoothing_window=smoothing_window)
+
+            model_performance_list.append(true_reward)
+            model_performance_nn.append(nn_reward)
+
             print('Completed agent-svf sampling.')
+            #print('True reward :', true_reward)
+            #print('NN reward :', nn_reward)
 
             #np.random.seed(11)
             #test_agent_svf = self.agent_svf_sampling(num_of_samples=300,
@@ -537,16 +567,23 @@ class DeepMaxEnt():
 
             # GRAD AND BACKPROP
 
-            loss, dot_prod, l1val , rewards_norm = self.calculate_grads(self.optimizer, state_rewards, diff_freq)
+            loss, dot_prod, l1val, reward_nn_grad_magnitude, rewards_norm = self.calculate_grads(self.optimizer, state_rewards,
+                                                                        diff_freq)
+
 
             lossList.append(loss)
             dot_prod_list.append(dot_prod)
             l1_reg_list.append(l1val)
             rewards_norm_list.append(rewards_norm)
-            
+            reward_grad_norm_list.append(reward_nn_grad_magnitude)
+
+
             self.plot_info((lossList, svf_diff_list, 
-                            l1_reg_list, dot_prod_list, rewards_norm_list))
+                            l1_reg_list, dot_prod_list, rewards_norm_list, reward_grad_norm_list,
+                            model_performance_list, model_performance_nn))
+
             self.optimizer.step()
+            self.lr_scheduler.step()
 
             print('done')
 
@@ -572,6 +609,18 @@ class DeepMaxEnt():
                 
                 plt.figure(4)
                 file_name = self.plot_save_folder+'rewards-norm'+str(i)+'.jpg'
+                plt.savefig(file_name)
+
+                plt.figure(5)
+                file_name = self.plot_save_folder+'reward-net-grad-norm'+str(i)+'.jpg'
+                plt.savefig(file_name)
+
+                plt.figure(6)
+                file_name = self.plot_save_folder+'model-performance-true'+str(i)+'.jpg'
+                plt.savefig(file_name)
+
+                plt.figure(7)
+                file_name = self.plot_save_folder+'model-performance-nn'+str(i)+'.jpg'
                 plt.savefig(file_name)
 
 

irlmethods/irlUtils.py
@@ -608,24 +608,31 @@ def calculate_svf_from_sampling(no_of_samples=1000, env=None,
     calculating the state visitation frequency from sampling. This function
     returns a dictionary, where the keys consists of only the states that has been
     visited and their corresponding values are the visitation frequency
+
+    ##update
+    Also returns the mean true and mean reward according to the current reward network 
     '''
     eps = 0.000001
     if feature_extractor is None:
         print('Featrue extractor missing. Exiting.')
         return None
 
-    rewards = np.zeros(no_of_samples)
+    rewards_true = np.zeros(no_of_samples) #the true rewards
+    rewards = np.zeros(no_of_samples) #the reward according to the reward network if present
+
     norm_factor = np.zeros(no_of_samples)
 
     svf_dict_list = []
     for i in range(no_of_samples):
+
         run_reward = 0
+        run_reward_true = 0
         current_svf_dict = {}
         state = env.reset()
         #print('agent position:', state['agent_state'])
         state = feature_extractor.extract_features(state)
         current_svf_dict[feature_extractor.hash_function(state)] = 1
-
+        #print('episode len', episode_length)
         for t in range(episode_length):
 
             action = select_action(policy_nn, state)
@@ -633,7 +640,7 @@ def calculate_svf_from_sampling(no_of_samples=1000, env=None,
             #print(action)
             state, reward, done,_ = env.step(action)
             #feature_extractor wraps the state in torch tensor so convert that back
-
+            run_reward_true += reward
             
             #get the state index
 
@@ -644,23 +651,28 @@ def calculate_svf_from_sampling(no_of_samples=1000, env=None,
                 current_svf_dict[feature_extractor.hash_function(state)] += 1*math.pow(gamma,t) 
                                                   
             if reward_nn is not None:
-                reward  = reward_nn(state)
+                nn_reward  = reward_nn(state)
+                run_reward+=nn_reward
 
-            run_reward+=reward
+            if done:
+                break
 
-        rewards[i] = run_reward
+        if reward_nn is not None:
+            rewards[i] = run_reward
+
+        rewards_true[i] = run_reward_true
         svf_dict_list.append(current_svf_dict)
 
     #rewards = rewards - np.min(rewards)+eps
     #changing it to the more generic exp
     #print('rewards non exp', rewards)
-    rewards = np.exp(rewards)
+    rewards_exp = np.exp(rewards)
     #print('Rewards :',rewards)
-    total_reward = sum(rewards)
+    total_reward_exp = sum(rewards_exp)
 
     #putting a control on the reweighting as discussed.
     if scale_svf:
-        weights = rewards/total_reward
+        weights = rewards_exp/total_reward_exp
     else:
         weights = np.ones(no_of_samples)
     #print('weights from svf_dict:',weights)
@@ -688,7 +700,11 @@ def calculate_svf_from_sampling(no_of_samples=1000, env=None,
             else:
                 master_dict[key] += dictionary[key]*weights[i]/norm_factor[i]
 
-    return collections.OrderedDict(sorted(master_dict.items()))
+
+
+    #print(rewards)
+    #print(rewards_true)
+    return collections.OrderedDict(sorted(master_dict.items())), np.mean(rewards_true), np.mean(rewards)
 
 
 

test_meIRL/run_experiment_rl.py
@@ -17,6 +17,10 @@ import torch.multiprocessing as mp
 from rlmethods.b_actor_critic import ActorCritic
 import re
 
+
+import psutil
+process = psutil.Process(os.getpid())
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--policy-path', type=str, nargs='?', default=None)
 parser.add_argument('--reward-path' , type=str, nargs='?', default= None)
@@ -37,6 +41,25 @@ parser.add_argument('--feat-extractor', type=str, default=None, help='The name o
 parser.add_argument('--state-discretization', type=int, default=128, help='The number of discrete \
                     parts you want to break the state')
 numbers = re.compile(r'(\d+)')
+
+def display_memory_usage(memory_in_bytes):
+
+    units = ['B', 'KB', 'MB', 'GB', 'TB']
+    mem_list = []
+    cur_mem = memory_in_bytes
+    while cur_mem > 1024:
+
+        mem_list.append(cur_mem%1024)
+        cur_mem /= 1024
+    
+    mem_list.append(cur_mem)
+    for i in range(len(mem_list)):
+
+        print(units[i] +':'+ str(mem_list[i])+', ', end='')
+
+    print('\n')
+
+
 
 def numericalSort(value):
     parts = numbers.split(value)
@@ -102,8 +125,8 @@ def main():
     experiment_logger.log_info(env.__dict__)
 
     model = ActorCritic(env, feat_extractor=feat_ext,  gamma=0.99,
-                        log_interval=100,max_ep_length=1000, hidden_dims=args.policy_net_hidden_dims,
-                        max_episodes=3000)
+                        log_interval=10, max_ep_length=2000, hidden_dims=args.policy_net_hidden_dims,
+                        max_episodes=600)
 
     experiment_logger.log_header('Details of the RL method :')
     experiment_logger.log_info(model.__dict__)
@@ -136,6 +159,9 @@ def main():
             model.policy.save('./saved-models/')
 
     if args.play:
+        xaxis = []
+        counter = 1
+
         for policy_file in policy_file_list:
 
             model.policy.load(policy_file)
@@ -147,7 +173,8 @@ def main():
 
         #plotting the 2d list
 
-            xaxis = np.arange(len(reward_across_models))
+            xaxis.append(counter)
+            counter += 1
             reward_across_models_np = np.array(reward_across_models)
             mean_rewards = np.mean(reward_across_models_np, axis=1)
             std_rewards = np.std(reward_across_models_np, axis=1)
@@ -156,6 +183,14 @@ def main():
                         mean_rewards+std_rewards, alpha = 0.5, facecolor = 'r')
             plt.draw()
             plt.pause(0.001)
+            '''
+            print('RAM usage :')
+            display_memory_usage(process.memory_info().rss)
+            print('GPU usage :')
+            display_memory_usage(torch.cuda.memory_allocated())
+            torch.cuda.empty_cache()
+            display_memory_usage(torch.cuda.memory_allocated())
+            '''
             #plt.show()
         plt.show()
     if args.play_user: