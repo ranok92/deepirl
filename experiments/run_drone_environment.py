f17051900995c6b02b251c3ab7229a0d49052ea3
Abhisek Konar <konarabhisek@gmail.com>
2019-10-16 15:29:38 -0400

    Restructed the run_drone_environment.py and fixed issues with crash_analysis and reward_analysis.

--- experiments/run_drone_environment.py
+++ experiments/run_drone_environment.py
@@ -71,7 +71,8 @@ parser.add_argument('--save-folder', type=str, default=None, help= 'The name of 
 #************************************************************************#
 
 parser.add_argument('--reward-analysis', action='store_true', default=False)
-
+parser.add_argument('--crash-analysis', action='store_true', default=False)
+parser.add_argument('--plain-run', action='store_true', default=True)
 
 def check_parameters(args):
 
@@ -93,165 +94,168 @@ def check_parameters(args):
 
 
 
-def main():
 
-    #**************************************************
-    thresh1 = 15
-    thresh2 = 30
+#**************************************************
+thresh1 = 10
+thresh2 = 15
 
-    step_size = 2
-    agent_width = 10
-    obs_width = 10
-    grid_size = 10
-
-
-    #**************************************************
-    ts=time.time()
-    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-
-    args = parser.parse_args()
-
-    #checks if all the parameters are in order
-    check_parameters(args)
-
-    if args.on_server:
-
-        matplotlib.use('Agg')
-        os.environ['SDL_VIDEODRIVER'] = 'dummy'
-
-    #*************************************************
-    #initialize information collector
-    from envs.drone_env_utils import InformationCollector
-
-    info_collector = InformationCollector(run_info=args.agent_type,
-                                          thresh=thresh2*step_size,
-                                          plot_info=args.save_plots,
-                                          store_info=args.store_results,
-                                         )
-
-    #*************************************************
-    #initialize environment
-    from envs.gridworld_drone import GridWorldDrone
-
-    consider_heading = True
-    if args.agent_type =='Potential_field':
-        consider_heading = False
-    env = GridWorldDrone(display=args.render, is_onehot = False, 
-                        seed=args.seed, obstacles=None, 
-                        show_trail=False,
-                        is_random=False,
-                        subject=args.subject,
-                        annotation_file=args.annotation_file,
-                        tick_speed=30, 
-                        obs_width=10,
-                        step_size=step_size,
-                        agent_width=agent_width,
-                        external_control=True,
-                        replace_subject=args.run_exact,
-                        show_comparison=True,
-                        consider_heading=True,
-                        show_orientation=True,
-                        rows=576, cols=720, width=grid_size)
+step_size = 2
+agent_width = 10
+obs_width = 10
+grid_size = 10
 
 
-    print('Environment initalized successfully.')
+#**************************************************
+ts=time.time()
+st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 
-    #*************************************************
-    #initialize the feature extractor
-    from featureExtractor.drone_feature_extractor import DroneFeatureSAM1, DroneFeatureMinimal
-    from featureExtractor.drone_feature_extractor import DroneFeatureOccup, DroneFeatureRisk
-    from featureExtractor.drone_feature_extractor import DroneFeatureRisk_v2
-    if args.feat_extractor == 'DroneFeatureSAM1':
+args = parser.parse_args()
 
-        feat_ext = DroneFeatureSAM1(agent_width=agent_width,
-                                    obs_width=obs_width,
-                                    step_size=step_size,
-                                    grid_size=grid_size,
-                                    thresh1=thresh1, thresh2=thresh2)
-    
-    if args.feat_extractor == 'DroneFeatureOccup':
+#checks if all the parameters are in order
+check_parameters(args)
 
-        feat_ext = DroneFeatureOccup(agent_width=agent_width,
-                                     obs_width=obs_width,
-                                     step_size=step_size,
-                                     grid_size=grid_size,
-                                     window_size=window_size)
+if args.on_server:
 
+    matplotlib.use('Agg')
+    os.environ['SDL_VIDEODRIVER'] = 'dummy'
 
-    if args.feat_extractor == 'DroneFeatureRisk':
+#*************************************************
+#initialize information collector
+from envs.drone_env_utils import InformationCollector
 
-        feat_ext = DroneFeatureRisk(agent_width=agent_width,
-                                    obs_width=obs_width,
-                                    step_size=step_size,
-                                    grid_size=grid_size,
-                                    show_agent_persp=True,
-                                    thresh1=thresh1, thresh2=thresh2)
+info_collector = InformationCollector(run_info=args.agent_type,
+                                      thresh=thresh2*step_size,
+                                      plot_info=args.save_plots,
+                                      store_info=args.store_results,
+                                     )
 
+#*************************************************
+#initialize environment
+from envs.gridworld_drone import GridWorldDrone
 
-    if args.feat_extractor == 'DroneFeatureRisk_v2':
-
-        feat_ext = DroneFeatureRisk_v2(agent_width=agent_width,
-                                       obs_width=obs_width,
-                                       step_size=step_size,
-                                       grid_size=grid_size,
-                                       show_agent_persp=True,
-                                       thresh1=thresh1, thresh2=thresh2)
-
-
-    #*************************************************
-    #initialize the agent
-
-
-    if args.agent_type=='Policy_network':
-        #initialize the network
-        from rlmethods.b_actor_critic import ActorCritic
-
-        agent = ActorCritic(env, feat_extractor=feat_ext,  gamma=1,
-                            log_interval=100,max_ep_length=args.max_ep_length,
-                            hidden_dims=args.policy_net_hidden_dims,
-                            )
-
-        if args.policy_path:
-
-            agent.policy.load(args.policy_path)
-
-        else:
-
-            print('Provide a policy path')
+consider_heading = True
+if args.agent_type =='Potential_field':
+    consider_heading = False
+env = GridWorldDrone(display=args.render, is_onehot = False, 
+                    seed=args.seed, obstacles=None, 
+                    show_trail=False,
+                    is_random=False,
+                    subject=args.subject,
+                    annotation_file=args.annotation_file,
+                    tick_speed=30, 
+                    obs_width=10,
+                    step_size=step_size,
+                    agent_width=agent_width,
+                    external_control=True,
+                    replace_subject=args.run_exact,
+                    show_comparison=True,
+                    consider_heading=consider_heading,
+                    show_orientation=True,
+                    rows=576, cols=720, width=grid_size)
 
 
-    if args.agent_type=='Potential_field':
-        #initialize the PF agent
-        from alternateController.potential_field_controller import PotentialFieldController as PFController
+print('Environment initalized successfully.')
 
-        attr_mag = 5
-        rep_mag = 3
-        agent = PFController(attr_f_limit=attr_mag,
-                             rep_f_limit=rep_mag)
+#*************************************************
+#initialize the feature extractor
+from featureExtractor.drone_feature_extractor import DroneFeatureSAM1, DroneFeatureMinimal
+from featureExtractor.drone_feature_extractor import DroneFeatureOccup, DroneFeatureRisk
+from featureExtractor.drone_feature_extractor import DroneFeatureRisk_v2
+if args.feat_extractor == 'DroneFeatureSAM1':
 
-    if args.agent_type=='Default':
+    feat_ext = DroneFeatureSAM1(agent_width=agent_width,
+                                obs_width=obs_width,
+                                step_size=step_size,
+                                grid_size=grid_size,
+                                thresh1=thresh1, thresh2=thresh2)
 
-        #the person from the video
-        pass
+if args.feat_extractor == 'DroneFeatureOccup':
 
-    #*************************************************
-    #load reward network if present
+    feat_ext = DroneFeatureOccup(agent_width=agent_width,
+                                 obs_width=obs_width,
+                                 step_size=step_size,
+                                 grid_size=grid_size,
+                                 window_size=window_size)
 
-    if args.reward_path is not None:
-        from irlmethods.deep_maxent import RewardNet 
 
-        state_size = feat_ext.extract_features(env.reset()).shape[0]
-        reward_net = RewardNet(state_size, args.reward_net_hidden_dims)
-        reward_net.load(args.reward_path)
+if args.feat_extractor == 'DroneFeatureRisk':
 
-    #*************************************************
-    #play
+    feat_ext = DroneFeatureRisk(agent_width=agent_width,
+                                obs_width=obs_width,
+                                step_size=step_size,
+                                grid_size=grid_size,
+                                show_agent_persp=True,
+                                thresh1=thresh1, thresh2=thresh2)
+
+
+if args.feat_extractor == 'DroneFeatureRisk_v2':
+
+    feat_ext = DroneFeatureRisk_v2(agent_width=agent_width,
+                                   obs_width=obs_width,
+                                   step_size=step_size,
+                                   grid_size=grid_size,
+                                   show_agent_persp=True,
+                                   thresh1=thresh1, thresh2=thresh2)
+
+
+#*************************************************
+#initialize the agent
+
+
+if args.agent_type=='Policy_network':
+    #initialize the network
+    from rlmethods.b_actor_critic import ActorCritic
+
+    agent = ActorCritic(env, feat_extractor=feat_ext,  gamma=1,
+                        log_interval=100,max_ep_length=args.max_ep_length,
+                        hidden_dims=args.policy_net_hidden_dims,
+                        )
+
+    if args.policy_path:
+
+        agent.policy.load(args.policy_path)
+
+    else:
+
+        print('Provide a policy path')
+
+
+if args.agent_type=='Potential_field':
+    #initialize the PF agent
+    from alternateController.potential_field_controller import PotentialFieldController as PFController
+
+    attr_mag = 3
+    rep_mag = 2
+    agent = PFController(attr_f_limit=attr_mag,
+                         rep_f_limit=rep_mag)
+
+if args.agent_type=='Default':
+
+    #the person from the video
+    pass
+
+#*************************************************
+#load reward network if present
+
+if args.reward_path is not None:
+    from irlmethods.deep_maxent import RewardNet 
+
+    state_size = feat_ext.extract_features(env.reset()).shape[0]
+    reward_net = RewardNet(state_size, args.reward_net_hidden_dims)
+    reward_net.load(args.reward_path)
+
+#*************************************************
+#play
+
+def reward_analysis():
+
     for i in range(args.num_trajs):
 
         #reset the world
         state=env.reset()
 
         if args.feat_extractor is not None:
+            feat_ext.reset()
             state_feat = feat_ext.extract_features(state)
             #pass
         #reset the information collector
@@ -271,23 +275,25 @@ def main():
 
                         action = i 
                         state, reward_true, _ , _ = env.step(action)
-                        #pdb.set_trace()
                         print('Taking a step', action)
                         if args.feat_extractor is not None:
 
-                            state_feat = feat_ext.extract_features(state, ignore_cur_state=True)
-                            reward_arr[i] = reward_net(state_feat)
+                            state_feat_temp = feat_ext.extract_features(state)
+                            reward_arr[i] = reward_net(state_feat_temp)
                             reward_arr_true[i] = reward_true
-                            env.rollback(1)
+                            state = env.rollback(1)
+                            state_feat = feat_ext.rollback(2, state)
                             #print(reward_arr)
                 #**********************************************
                 #making sure the graphics are consistent
 
-                state_feat = feat_ext.extract_features(state)
+                #if t>0: #skip if this is the first frame
+                #    state_feat = feat_ext.extract_features(state)
                 #**********************************************
                 #selecting the action
                 #action selection for network
                 if args.agent_type=='Policy_network':
+                    #pdb.set_trace()
                     action, probs = agent.select_action_play(state_feat)
                 else:
                 #action selection for alternate controller namely potential field
@@ -309,7 +315,7 @@ def main():
                     plt.plot(probs.cpu().detach().numpy(), c='g')
                     #action = np.argmax(true_reward_norm)
                     #print('Action taken from here:', action)
-                    plt.show()
+
                     #comparing the policy network
 
 
@@ -320,16 +326,16 @@ def main():
                 action = agent.select_action_play(state) 
             #pdb.set_trace()
             state, reward, done, _ = env.step(action)
-
+           
             if args.feat_extractor is not None:
-                state_feat = feat_ext.extract_features(state, ignore_cur_state=True)
+                state_feat = feat_ext.extract_features(state)
             if args.reward_path is not None:
                 reward = reward_net(state_feat)
 
-            if args.reward_analysis:
-                print('Reward : {} for action {}:'.format(reward, action))
+            #if args.reward_analysis:
+            print('Reward : {} for action {}:'.format(reward, action))
                 #pdb.set_trace()
-
+            plt.show()
             info_collector.collect_information_per_frame(state)
 
             t+=1
@@ -340,5 +346,99 @@ def main():
     info_collector.plot_information()
 
 
+def crash_analysis():
+
+    for i in range(args.num_trajs):
+
+        #reset the world
+        crash_analysis = False
+        state=env.reset()
+        print('Current subject :', env.cur_ped)
+        if args.feat_extractor is not None:
+            feat_ext.reset()
+            state_feat = feat_ext.extract_features(state)
+            #pass
+        #reset the information collector
+        info_collector.reset_info(state)
+        done=False
+        t = 0
+        while t < args.max_ep_length and not done:
+
+            if args.feat_extractor is not None:
+
+            
+                if args.agent_type=='Policy_network':
+                    action, probs = agent.select_action_play(state_feat)
+                else:
+                #action selection for alternate controller namely potential field
+                    action = agent.select_action_play(state)
+
+                if args.render:
+                    feat_ext.overlay_bins(state)
+
+            else:
+                action = agent.select_action_play(state) 
+            #pdb.set_trace()
+            state, reward_true, done, _ = env.step(action)
+            
+           
+            if args.feat_extractor is not None:
+                state_feat = feat_ext.extract_features(state)
+
+                if crash_analysis:
+                    pdb.set_trace()
+            if args.reward_path is not None:
+                reward = reward_net(state_feat)
+            else:
+                reward = reward_true
+
+            #if args.reward_analysis:
+            print('Reward : {} for action {}:'.format(reward, action))
+                #pdb.set_trace()
+
+            if done:
+                print('Crash frame : ', env.current_frame)
+                print('Agent position history :')
+                for i in range(len(feat_ext.agent_state_history)):
+                    print(feat_ext.agent_state_history[i]['position'], env.heading_dir_history[i])
+                if args.crash_analysis:
+                    if  reward_true < -0.5:
+                        if not crash_analysis:
+                            if t > 3:
+                                state = env.rollback(3)
+                                state_feat = feat_ext.rollback(4, state)
+                            else:
+                                state = env.rollback(t-1)
+                                state_feat = feat_ext.rollback(t, state)
+                            print('Current frame after rollback :', env.current_frame)
+                            for i in range(len(feat_ext.agent_state_history)):
+                                print(feat_ext.agent_state_history[i]['position'],env.heading_dir_history[i])                            
+                            done=False
+                            crash_analysis=True
+                        else:
+                            break
+                else:
+                    break
+
+            #info_collector.collect_information_per_frame(state)
+
+            t+=1
+
+        #info_collector.collab_end_traj_results()
+
+    #info_collector.collab_end_results()
+    #info_collector.plot_information()
+
+
+
+
+
 if __name__ == '__main__':
-    main()
+
+
+    if args.reward_analysis:
+        reward_analysis()
+    else:
+        crash_analysis()
+
+
