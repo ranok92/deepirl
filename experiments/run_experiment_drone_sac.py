c01e17364ca96d1ae007dd7586b1bf9bde9fa397
Abhisek Konar <konarabhisek@gmail.com>
2019-11-09 11:21:29 -0500

    Fiddling with different RL methods.

---
+++ experiments/run_experiment_drone_sac.py
@@ -0,0 +1,336 @@
+import pdb
+import sys  # NOQA
+sys.path.insert(0, '..')  # NOQA: E402
+
+import numpy as np
+import argparse
+import torch.multiprocessing as mp
+import os
+
+import glob
+from logger.logger import Logger
+import matplotlib
+import datetime, time
+#from debugtools import compile_results
+from utils import step_wrapper, reset_wrapper
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--policy-path', type=str, nargs='?', default=None)
+parser.add_argument('--reward-path' , type=str, nargs='?', default= None)
+parser.add_argument('--play', action='store_true',
+                    help='play given or latest stored policy.')
+parser.add_argument('--play-user' , action='store_true' , 
+                    help='lets the user play the game')
+parser.add_argument('--dont-save', action='store_true',
+                   help="don't save the policy network weights.")
+parser.add_argument('--render', action='store_true', help="show the env.")
+parser.add_argument('--num-trajs', type=int, default=10)
+parser.add_argument('--view-reward', action='store_true')
+
+parser.add_argument('--policy-net-hidden-dims', nargs="*", type=int, default=[128])
+parser.add_argument('--reward-net-hidden-dims', nargs="*", type=int, default=[128])
+
+parser.add_argument('--lr', type=float, default=0.001)
+parser.add_argument('--on-server', action='store_true')
+
+parser.add_argument('--feat-extractor', type=str, default=None, help='The name of the \
+                     feature extractor to be used in the experiment.')
+parser.add_argument('--save-folder', type=str, default=None, help= 'The name of the folder to \
+                    store experiment related information.')
+
+parser.add_argument('--annotation-file', type=str, default='../envs/expert_datasets/university_students/annotation/processed/frame_skip_1/students003_processed_corrected.txt', help='The location of the annotation file to \
+                    be used to run the environment.')
+
+parser.add_argument('--total-episodes', type=int, default=1000, help='Total episodes of RL')
+parser.add_argument('--max-ep-length', type=int, default=200, help='Max length of a single episode.')
+
+parser.add_argument('--replace-subject', action='store_true')
+parser.add_argument('--seed', type=int, default=789)
+
+parser.add_argument('--subject', type=int, default=None, help='The id of the pedestrian to replace during training or \
+                    testing.')
+
+parser.add_argument('--exp-trajectory-path', type=str, default=None, help='The name of the directory in which \
+                    the expert trajectories are stored.(Relative path)')
+
+parser.add_argument('--segment-size', type=int, default=None, help='Size of each trajectory segment.')
+def main():
+    
+    #####for the logger
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    ###################
+
+    args = parser.parse_args()
+
+    if args.on_server:
+
+        matplotlib.use('Agg')
+        # pygame without monitor
+        os.environ['SDL_VIDEODRIVER'] = 'dummy'
+
+    from matplotlib import pyplot as plt
+    mp.set_start_method('spawn')
+
+    from rlmethods.scott_SAC.SAC import SAC
+    from envs.gridworld_drone import GridWorldDrone
+    from featureExtractor.drone_feature_extractor import DroneFeatureSAM1, DroneFeatureOccup, DroneFeatureRisk, DroneFeatureRisk_v2
+    from featureExtractor.gridworld_featureExtractor import FrontBackSide,LocalGlobal,OneHot,SocialNav,FrontBackSideSimple
+    from featureExtractor.drone_feature_extractor import DroneFeatureRisk_speed
+
+    save_folder = None
+
+
+    if not args.dont_save and not args.play:
+
+        if not args.save_folder:
+            print('Provide save folder.')
+            exit()
+
+        policy_net_dims = '-policy_net-'
+        for dim in args.policy_net_hidden_dims:
+            policy_net_dims += str(dim) 
+            policy_net_dims += '-'
+
+        reward_net_dims = '-reward_net-'
+        for dim in args.reward_net_hidden_dims:
+            reward_net_dims += str(dim)
+            reward_net_dims += '-'
+
+        save_folder = './results/'+ args.save_folder +st + args.feat_extractor + \
+                      '-seed-'+str(args.seed) + policy_net_dims + reward_net_dims + \
+                      '-total-ep-'+str(args.total_episodes)+'-max-ep-len-'+ str(args.max_ep_length)
+
+        experiment_logger = Logger(save_folder,'experiment_info.txt')
+        experiment_logger.log_header('Arguments for the experiment :')
+        experiment_logger.log_info(vars(args))
+    
+    window_size = 9
+    step_size = 2
+    agent_width = 10
+    obs_width = 10
+    grid_size = 10
+
+    feat_ext = None
+    #initialize the feature extractor to be used
+    if args.feat_extractor == 'Onehot':
+        feat_ext = OneHot(grid_rows = 10 , grid_cols = 10)
+    if args.feat_extractor == 'SocialNav':
+        feat_ext = SocialNav(fieldList = ['agent_state','goal_state'])
+    if args.feat_extractor == 'FrontBackSideSimple':
+        feat_ext = FrontBackSideSimple(thresh1 = 1,
+                                    thresh2 = 2,
+                                    thresh3 = 3,
+                                    thresh4=4,
+                                    step_size=step_size,
+                                    agent_width=agent_width,
+                                    obs_width=obs_width,
+                                    )
+
+    if args.feat_extractor == 'LocalGlobal':
+        feat_ext = LocalGlobal(window_size=11, grid_size=grid_size,
+                           agent_width=agent_width, 
+                           obs_width=obs_width,
+                           step_size=step_size,
+                           )
+    
+    if args.feat_extractor == 'DroneFeatureSAM1':
+
+        feat_ext = DroneFeatureSAM1(agent_width=agent_width,
+                                    obs_width=obs_width,
+                                    step_size=step_size,
+                                    grid_size=grid_size,
+                                    thresh1=15, thresh2=30)
+    
+    if args.feat_extractor == 'DroneFeatureOccup':
+
+        feat_ext = DroneFeatureOccup(agent_width=agent_width,
+                                     obs_width=obs_width,
+                                     step_size=step_size,
+                                     grid_size=grid_size,
+                                     window_size=window_size)
+
+
+    if args.feat_extractor == 'DroneFeatureRisk':
+
+        feat_ext = DroneFeatureRisk(agent_width=agent_width,
+                                    obs_width=obs_width,
+                                    step_size=step_size,
+                                    grid_size=grid_size,
+                                    show_agent_persp=True,
+                                    thresh1=15, thresh2=30)
+
+
+    if args.feat_extractor == 'DroneFeatureRisk_v2':
+
+        feat_ext = DroneFeatureRisk_v2(agent_width=agent_width,
+                                       obs_width=obs_width,
+                                       step_size=step_size,
+                                       grid_size=grid_size,
+                                       show_agent_persp=True,
+                                       thresh1=15, thresh2=30)
+
+    if args.feat_extractor == 'DroneFeatureRisk_speed':
+
+        feat_ext = DroneFeatureRisk_speed(agent_width=agent_width,
+                                       obs_width=obs_width,
+                                       step_size=step_size,
+                                       grid_size=grid_size,
+                                       show_agent_persp=False,
+                                       thresh1=10, thresh2=15)
+
+
+
+    if feat_ext is None:
+        print('Please enter proper feature extractor!')
+        exit()
+    #log feature extractor info
+
+    if not args.dont_save and not args.play:
+
+        experiment_logger.log_header('Parameters of the feature extractor :')
+        experiment_logger.log_info(feat_ext.__dict__)
+
+    #initialize the environment
+    if args.replace_subject:
+        replace_subject=True
+    else:
+        replace_subject=False
+
+    env = GridWorldDrone(display=args.render, is_onehot = False, 
+                        seed=args.seed, obstacles=None, 
+                        show_trail=False,
+                        is_random=True,
+                        annotation_file=args.annotation_file,
+                        subject=args.subject,
+                        tick_speed=60, 
+                        obs_width=10,
+                        step_size=step_size,
+                        agent_width=agent_width,
+                        replace_subject=replace_subject,
+                        segment_size=args.segment_size,
+                        external_control=True,
+                        step_reward=0.001,
+                        show_comparison=True,
+                        consider_heading=True,
+                        show_orientation=True,
+
+                        #rows=200, cols=300, width=grid_size)                       
+                        rows=576, cols=720, width=grid_size)
+
+    #log environment info
+    if not args.dont_save and not args.play:
+
+        experiment_logger.log_header('Environment details :')
+        experiment_logger.log_info(env.__dict__)
+
+    #initialize RL 
+    model = SAC(env, 
+                feat_extractor=feat_ext,  
+                log_interval=100,max_ep_length=args.max_ep_length,
+                hidden_dims=args.policy_net_hidden_dims,
+                save_folder=save_folder, 
+                max_episodes = args.total_episodes)
+
+    #log RL info
+    if not args.dont_save and not args.play:
+
+        experiment_logger.log_header('Details of the RL method :')
+        experiment_logger.log_info(model.__dict__)
+    
+
+
+    if args.policy_path is not None:
+
+        from debugtools import numericalSort
+        policy_file_list =  []
+        reward_across_models = []
+        if os.path.isfile(args.policy_path):
+            policy_file_list.append(args.policy_path)
+        if os.path.isdir(args.policy_path):
+            policy_names = glob.glob(os.path.join(args.policy_path, '*.pt'))
+            policy_file_list = sorted(policy_names, key=numericalSort)
+
+        xaxis = np.arange(len(policy_file_list))
+
+
+        
+    if not args.play and not args.play_user:
+        #no playing of any kind, so training
+
+        if args.reward_path is None:
+            if args.policy_path:
+                model.policy.load(args.policy_path)
+            model.train()
+        else:
+            from irlmethods.deep_maxent import RewardNet
+            state_size = feat_ext.extract_features(env.reset()).shape[0]
+            reward_net = RewardNet(state_size, args.reward_net_hidden_dims)
+            reward_net.load(args.reward_path)
+            print(next(reward_net.parameters()).is_cuda)
+            model.train(reward_net = reward_net)
+
+        if not args.dont_save:  
+            model.policy.save(save_folder+'/policy-models/')
+
+
+    if args.play:
+        #env.tickSpeed = 15
+        from debugtools import compile_results
+        xaxis = []
+        counter = 1
+        plt.figure(0)
+        avg_reward_list = []
+        frac_good_run_list = []
+        for policy_file in policy_file_list:
+
+            print('Playing for policy :', policy_file)
+            model.policy.load(policy_file)
+            policy_folder = policy_file.strip().split('/')[0:-2]
+            save_folder = ''
+            for p in policy_folder:
+                save_folder = save_folder + p + '/'
+
+            print('The final save folder ', save_folder)
+            #env.tickSpeed = 10
+            assert args.policy_path is not None, 'pass a policy to play from!'
+            if args.exp_trajectory_path is not None:
+                from irlmethods.irlUtils import calculate_expert_svf
+                expert_svf = calculate_expert_svf(args.exp_trajectory_path,
+                                                  max_time_steps=args.max_ep_length,
+                                                  feature_extractor=feat_ext,
+                                                  gamma=1)
+            #reward_across_models.append(model.generate_trajectory(args.num_trajs, args.render))
+            if args.exp_trajectory_path is None:
+
+                if args.dont_save:
+                    rewards, state_info, sub_info = model.generate_trajectory(args.num_trajs, args.render)
+                else:
+                    rewards, state_info, sub_info = model.generate_trajectory(args.num_trajs, args.render, path=save_folder+'/agent_generated_trajectories/')
+            else:
+
+                if args.dont_save:
+                    rewards, state_info, sub_info  = model.generate_trajectory(args.num_trajs, args.render,
+                                              expert_svf=expert_svf)
+                else:
+                    rewards, state_info, sub_info  = model.generate_trajectory(args.num_trajs, args.render, 
+                                              path=save_folder+'/agent_generated_trajectories/',
+                                              expert_svf=expert_svf)
+
+            avg_reward, good_run_frac = compile_results(rewards, state_info, sub_info)
+            #pdb.set_trace()
+            avg_reward_list.append(avg_reward)
+            frac_good_run_list.append(good_run_frac)
+            plt.plot(avg_reward_list, c='r')
+            plt.plot(frac_good_run_list, c='g')
+            plt.draw()
+        plt.show()
+        
+
+    if args.play_user:
+        env.tickSpeed = 200
+
+        model.generate_trajectory_user(args.num_trajs, args.render , path='./user_generated_trajectories/')
+
+if __name__ == '__main__':
+    main()
---
+++ rlmethods/scott_SAC/SAC.py
@@ -0,0 +1,362 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+#from torch.distributions import Normal
+import math
+import pdb
+from rlmethods.scott_SAC import sac_utils
+from itertools import count
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+# Returns an action for a given state
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, max_action):
+        super(Actor, self).__init__()
+
+        self.l1 = nn.Linear(state_dim, 256)
+        self.l2 = nn.Linear(256, 256)
+        
+        self.mean = nn.Linear(256, action_dim)
+        self.log_std = nn.Linear(256, action_dim)
+
+        self.max_action = max_action
+        self.action_dim = action_dim
+
+    
+    def forward(self, x):
+        x = F.relu(self.l1(x))
+        x = F.relu(self.l2(x))
+        
+        mean = self.mean(x)
+        log_std = self.log_std(x).clamp(-4, 15)
+        std = log_std.exp()
+
+        z = mean + std * torch.FloatTensor(np.random.normal(0, 1, size=mean.size())).to(device) 
+        action = torch.tanh(z)
+
+        log_prob = (-math.log(2 * math.pi)/self.action_dim - torch.log(std.pow(2) + 1e-6)/self.action_dim - std.pow(-2) * (z - mean).pow(2)) * 0.5
+        
+        #log_prob = normal.log_prob(z) - torch.log(1 - action.pow(2) + 1e-6)
+        log_prob = log_prob - torch.log(1 - action.pow(2) + 1e-6)
+        log_prob = log_prob.sum(1, keepdim=True)
+        return self.max_action * action, log_prob, self.max_action * torch.tanh(mean)
+
+
+# Returns a Q-value for given state/action pair
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Critic, self).__init__()
+
+        self.l1 = nn.Linear(state_dim + action_dim, 256)
+        self.l2 = nn.Linear(256, 256)
+        self.l3 = nn.Linear(256, 1)
+
+        self.l4 = nn.Linear(state_dim + action_dim, 256)
+        self.l5 = nn.Linear(256, 256)
+        self.l6 = nn.Linear(256, 1)
+
+
+    def forward(self, x, u):
+        xu = torch.cat([x, u], 1)
+        q1 = F.relu(self.l1(xu))
+        q1 = F.relu(self.l2(q1))
+        q1 = self.l3(q1)
+        
+        q2 = F.relu(self.l4(xu))
+        q2 = F.relu(self.l5(q2))
+        q2 = self.l6(q2)
+        return q1, q2
+
+
+    def q1(self, x, u):
+        q1 = F.relu(self.l1(torch.cat([x, u], 1)))
+        q1 = F.relu(self.l2(q1))
+        q1 = self.l3(q1)
+        return q1
+
+
+class Value(nn.Module):
+    def __init__(self, state_dim):
+        super(Value, self).__init__()
+
+        self.l1 = nn.Linear(state_dim, 256)
+        self.l2 = nn.Linear(256, 256)
+        self.l3 = nn.Linear(256, 1)
+
+
+    def forward(self, x):
+        x = F.relu(self.l1(x))
+        x = F.relu(self.l2(x))
+        x = self.l3(x)
+        return x 
+
+
+class SAC(object):
+
+    def __init__(self, env,
+                 feat_extractor=None,
+                 policy=None,
+                 log_interval=10,
+                 max_episodes=1000,
+                 max_ep_length=200,
+                 hidden_dims=[256],
+                 save_folder=None,
+                 reward_scale=5):
+        
+        #adding more parameters to suit the Social navigation project
+
+        self.env = env
+        self.feature_extractor = feat_extractor 
+        self.log_interval = log_interval
+
+
+        self.max_ep_length = max_ep_length
+        self.max_episodes = max_episodes
+
+        if self.feature_extractor is not None:
+            state_dim = self.feature_extractor.extract_features(env.reset()).shape[0]
+
+        else:
+            state_dim = env.reset().shape[0] 
+
+        self.hidden_dims = hidden_dims
+
+        self.start_timesteps = 1000
+        #**have to fix the action dim thing
+        #not using hidden_dims for now
+
+        action_dim = 2
+        max_action = 1
+        self.orient_quantization = len(self.env.orientation_array)
+        self.speed_quantization = len(self.env.speed_array)
+
+        #############################################################
+
+        self.actor = Actor(state_dim, action_dim, max_action).to(device)
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=3e-4)
+
+        self.critic = Critic(state_dim, action_dim).to(device)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=3e-4)
+
+        self.value = Value(state_dim).to(device)
+        self.value_target = Value(state_dim).to(device)
+        self.value_target.load_state_dict(self.value.state_dict())
+        self.value_optimizer = torch.optim.Adam(self.value.parameters(), lr=3e-4)
+
+        self.criterion = nn.MSELoss()
+        self.state_dim = state_dim
+        self.reward_scale = reward_scale
+
+
+
+
+    def select_action(self, state, test=False):
+        with torch.no_grad():
+            state = torch.FloatTensor(state.cpu().reshape(1, -1)).to(device)
+            action, log_prob, mean_action = self.actor(state)
+            
+            #if test, select the most likely action
+            if test:
+                action = mean_action
+
+            action = action.squeeze()
+            orient = self.get_quantization(action[0].item(), -1, 2, self.orient_quantization)
+            speed = self.get_quantization(action[1].item(), -1, 2, self.speed_quantization)
+
+            action_env = speed*self.orient_quantization+orient
+
+            return action_env
+
+
+    def get_quantization(self, raw_inp, min_val, max_val, quantization):
+
+        range_val = max_val-min_val
+        return int((raw_inp-min_val)/range_val*quantization)
+
+
+    #the action for the environment is 1 dimensional but it comprises
+    #of two things, speed*orientation control.
+    #This, in the network is represnted as a 2 dim array
+    #but when passing the result into the environment is comverted
+    #to a single number by multiplication
+    def convert_env_to_network_action(self, env_action):
+        #the 2d array represents [orient, speed]
+        return np.asarray([env_action%self.orient_quantization, int(env_action/self.orient_quantization)])
+
+
+    def train(self, reward_network=None, irl=False):
+
+        replay_buffer = sac_utils.ReplayBuffer()
+        running_reward = 0
+        running_reward_list = []
+        total_timesteps = 0
+
+        for i_episode in count(1):
+
+            if self.feature_extractor is not None:
+
+                state = self.feature_extractor.extract_features(self.env.reset())
+            else:
+                state = self.env.reset()
+
+
+            ep_timestep = 0
+            ep_reward = 0
+            for i in range(self.max_ep_length):
+
+                if total_timesteps < self.start_timesteps:
+                    #random action
+                    action = self.env.action_space.sample()
+                else:
+                    action = self.select_action(state)
+
+
+                new_state, reward, done, _ = self.env.step(action)
+
+                if self.feature_extractor is not None:
+
+                    new_state = self.feature_extractor.extract_features(new_state)
+
+                if reward_network is not None:
+
+                    reward = reward_network(new_state).item()
+
+                ep_reward += reward
+                if done:
+                    done_bool=1
+                else:
+                    done_bool=0
+
+                action = self.convert_env_to_network_action(action)
+                replay_buffer.add((state.cpu(), new_state.cpu(), action, reward, done_bool))
+
+                if total_timesteps >= self.start_timesteps:
+
+                    self.finish_episode(replay_buffer, 1)
+
+                state = new_state
+                ep_timestep += 1
+                total_timesteps += 1
+
+                if done:
+                    break
+
+                running_reward += ep_reward
+
+                #logging intermediate training information
+
+                if not irl:
+
+                    if i_episode >=10 and i_episode %self.log_interval == 0:
+
+                        print('Ep {}\tLast length: {:5d}\tAvg. reward: {:.2f}'.format(
+                            i_episode, ep_timestep, running_reward/self.log_interval))
+                        print('The action frequency array :', action_array)
+                        running_reward_list.append(running_reward/self.log_interval)
+                        running_reward = 0
+                        action_array = np.zeros(self.env.action_space.n)
+
+                    if i_episode > self.max_episodes and self.max_episodes > 0:
+
+                        break
+
+                else:
+
+                    assert self.max_episodes > 0
+
+                    if i_episode >= 10 and  i_episode % self.log_interval == 0:
+                        print('Ep {}\tLast length: {:5d}\tAvg. reward: {:.2f}'.format(
+                            i_episode, ep_timestep, running_reward/self.log_interval))
+                        print('The action frequency array :', action_array)
+                        action_array = np.zeros(self.env.action_space.n)
+                        running_reward_list.append(running_reward/self.log_interval)
+
+                        running_reward = 0
+
+                    # terminate if max episodes exceeded
+                    if i_episode > self.max_episodes:
+                        break
+
+        if self.save_folder:
+            self.plot_and_save_info((loss_list, running_reward_list), ('Loss', 'rewards_obtained'))
+
+
+        return self.actor
+
+
+
+    def finish_episode(self, replay_buffer, iterations, batch_size=256, discount=0.99, tau=0.005): 
+
+        for it in range(iterations):
+
+            # Each of these are batches 
+            x, y, u, r, d = replay_buffer.sample(batch_size)
+            state = torch.FloatTensor(x).to(device)
+            action = torch.FloatTensor(u).to(device)
+            next_state = torch.FloatTensor(y).to(device)
+            done = torch.FloatTensor(1 - d).to(device)
+            reward = torch.FloatTensor(r).to(device)
+
+            # Target Q
+            with torch.no_grad():
+                target_Q = self.value_target(next_state)
+                target_Q = self.reward_scale * reward + done * discount * target_Q
+
+            # Current Q estimate
+            current_Q1, current_Q2 = self.critic(state, action)
+
+            # Compute critic loss
+            critic_loss = self.criterion(current_Q1, target_Q) + self.criterion(current_Q2, target_Q)
+
+            # Optimize the critic
+            self.critic_optimizer.zero_grad()
+            critic_loss.backward()
+            self.critic_optimizer.step()
+
+            # Compute actor loss
+            new_action, log_prob, _ = self.actor(state)
+            actor_loss = (log_prob - self.critic.q1(state, new_action)).mean()
+
+            # Optimize the actor 
+            self.actor_optimizer.zero_grad()
+            actor_loss.backward()
+            self.actor_optimizer.step()
+
+            # Target V
+            with torch.no_grad():
+                new_action, log_prob, _ = self.actor(state)
+                target_Q1, target_Q2 = self.critic(state, new_action)
+                target_V = torch.min(target_Q1, target_Q2) - log_prob
+
+            # Current V estimate
+            current_V = self.value(state)
+
+            # Compute value loss
+            value_loss = self.criterion(current_V, target_V)
+
+            # Optimize the critic
+            self.value_optimizer.zero_grad()
+            value_loss.backward()
+            self.value_optimizer.step()
+
+            # Update the frozen target models
+            for param, target_param in zip(self.value.parameters(), self.value_target.parameters()):
+                target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
+
+
+
+
+    def save(self, filename, directory):
+        torch.save(self.actor.state_dict(), f"{directory}/{filename}_actor.pth")
+        torch.save(self.critic.state_dict(), f"{directory}/{filename}_critic.pth")
+        torch.save(self.value.state_dict(), f"{directory}/{filename}_value.pth")
+
+    def load(self, filename, directory):
+        self.actor.load_state_dict(torch.load(f"{directory}/{filename}_actor.pth"))
+        self.critic.load_state_dict(torch.load(f"{directory}/{filename}_critic.pth"))
+        self.value.load_state_dict(torch.load(f"{directory}/{filename}_value.pth"))
+        self.value_target.load_state_dict(self.value.state_dict())
