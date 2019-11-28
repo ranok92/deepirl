7dd41541e2e85e47e02e2ead3571b1f2121d9ce3
Abhisek Konar <konarabhisek@gmail.com>
2019-11-28 08:38:32 -0500

    Adapted the potential field controller for the new environment with discretized speed and orientation actions.

--- alternateController/potential_field_controller.py
+++ alternateController/potential_field_controller.py
@@ -3,9 +3,29 @@ import pdb
 
 
 def unit_vector(vector):
-        """ Returns the unit vector of the vector.  """
-        return vector / np.linalg.norm(vector)
+    """ Returns the unit vector of the vector.  """
+    return vector / np.linalg.norm(vector)
 
+
+def get_rot_matrix(theta):
+    '''
+    returns the rotation matrix given a theta value
+    '''
+    return np.asarray([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
+
+
+def deg_to_rad(val):
+    '''
+    converts degree to radians
+    '''
+    return val*(np.pi)/180
+
+
+def rad_to_deg(val):
+    '''
+    converts radians to degrees
+    '''
+    return val*180/np.pi
 
 
 def angle_between(v1, v2):
@@ -29,17 +49,18 @@ def angle_between(v1, v2):
 
 class PotentialFieldController():
 
-    def __init__(self, k=5, v=1,
+    def __init__(self,
+                 speed_div,
+                 orient_div,
+                 orient_quant,
+                 k=5, v=1,
                  eta=200,
-                 sigma=100, 
-                 agent_width=10,
-                 obs_width=10,
+                 sigma=100,
                  c_limit=5,
                  attr_f_limit=5,
                  rep_f_limit=5,
                  rep_force_dist_limit=60,
                  force_threshold=0.5,
-                 consider_heading=True
                  ):
 
         self.KP = k
@@ -49,27 +70,42 @@ class PotentialFieldController():
         self.conic_limit = c_limit
         self.rep_force_dist_limit = rep_force_dist_limit
         self.pfield = None
+
+        '''
         self.action_array = [np.asarray([-1,0]),np.asarray([-1,1]),
                              np.asarray([0,1]),np.asarray([1,1]),
                              np.asarray([1,0]),np.asarray([1,-1]),
                              np.asarray([0,-1]),np.asarray([-1,-1]),
                              np.asarray([0,0])]
+        '''
 
-
-
+        self.orientation_array = []
+        self.speed_array = []
         self.attr_force_lim = attr_f_limit
         self.rep_force_lim = rep_f_limit
+        self.define_action_array(speed_div, orient_div, orient_quant)
+        
         self.globalpath = {}
         self.normalize_force = True
         self.eps = np.finfo(float).eps
         self.force_threshold = force_threshold
 
+    def define_action_array(self, speed_div, orient_div, orient_quant):
 
-    #calculates the attractive force the agent is experiencing given the 
-    #position of the agent and the goal
-    #returns a 2 dim vector
-    def calculate_attractive_force_btwpoints(self,agent_state,goal_state):
+        force_lim = self.attr_force_lim
+        self.speed_array = [i/speed_div*force_lim for i in range(speed_div)]
+        orient_range = int((orient_div-1)/2)
+        deg_array = [(deg_to_rad(i*orient_quant)) for i in range(-orient_range, orient_range, 1)]
+        unit_v = np.array([-1, 0])
+        rot_matrix_list = [get_rot_matrix(deg) for deg in deg_array]
+        self.orientation_array = [np.matmul(rot_matrix, unit_v) for rot_matrix in rot_matrix_list]
 
+    def calculate_attractive_force_btwpoints(self, agent_state, goal_state):
+        '''
+        calculates the attractive force the agent is experiencing given the 
+        position of the agent and the goal
+        returns a 2 dim vector
+        '''
         global_coord = agent_state['position']
         #pdb.set_trace()
         goal_vector = goal_state
@@ -77,7 +113,7 @@ class PotentialFieldController():
         if agent_state['orientation'] is None:
             agent_state['orientation'] = np.array([0, 0])
 
-        attr_force =  self.KP*(global_coord - goal_vector) - self.KV*agent_state['orientation']
+        attr_force = self.KP*(global_coord - goal_vector) - self.KV*agent_state['orientation']
         if self.normalize_force:
 
             mag = np.hypot(attr_force[0] , attr_force[1])/self.attr_force_lim
@@ -89,7 +125,7 @@ class PotentialFieldController():
 
     #calculates the repulsive force the agent experiences given its position and 
     #the position of the obstacles
-    def calculate_repulsive_force_btwpoints(self,agent_state,obs):
+    def calculate_repulsive_force_btwpoints(self, agent_state, obs):
 
         agent = agent_state['position']
         obs = obs['position']
@@ -103,7 +139,7 @@ class PotentialFieldController():
             if self.normalize_force:
                 return (force_vector_x/force_mag , force_vector_y/force_mag)
             else:
-                return (force_vector_x , force_vector_y)
+                return (force_vector_x, force_vector_y)
         else:
             return (0,0)
 
@@ -136,9 +172,12 @@ class PotentialFieldController():
         return pforce
 
 
-
+    '''
     def select_action_from_force(self, force):
-
+        
+        takes an action based on the action space of the environment
+        and the force being currently being applied on the agent
+        
         #if force is too small, it doesnt move
         if np.linalg.norm(force) < self.force_threshold:
             return 8
@@ -153,9 +192,31 @@ class PotentialFieldController():
             #print('similarity_index', similarity_index)
             #pdb.set_trace()
             return np.argmin(similarity_index)
+    '''
+    def select_action_from_force(self, force, state):
+        '''
+        takes an action based on the action space of the environment
+        and the force being currently being applied on the agent
+        '''
+        #if force is too small, it doesnt move
 
-    def select_action_play(self, state):
-        #given the current state take the apt action
+        #rotate the force based on the current_heading_dir
+        force_rel = np.matmul(get_rot_matrix(deg_to_rad(state['agent_head_dir'])), force)
+        similarity_index = np.zeros(len(self.orientation_array))
+        for i in range(len(self.orientation_array)):
+
+            similarity_index[i] = angle_between(force_rel, self.orientation_array[i])
+
+        #print('Action taken :', np.argmin(similarity_index))
+        #print('similarity_index', similarity_index)
+        #pdb.set_trace()
+        return int(np.argmin(similarity_index)), 3
+
+
+    def eval_action(self, state):
+        '''
+        given the current state take the apropriate action
+        '''
         total_force = 0
         agent_state = state['agent_state']
         goal_state = state['goal_state']
@@ -173,14 +234,15 @@ class PotentialFieldController():
             rep_force += rf
 
         total_force += rep_force
-        true_action = int(self.select_action_from_force(total_force))
-        
-        if true_action!=8:
-            rel_action = (true_action - state['agent_head_dir'])%8
+        orientation, speed = self.select_action_from_force(total_force, state)
+        '''
+        if orientation!=8:
+            rel_action = (orientation - state['agent_head_dir'])%8
         else:
             rel_action = true_action
         #pdb.set_trace()
-        return rel_action
+        '''
+        return speed*len(self.orientation_array)+orientation
 
 
 
