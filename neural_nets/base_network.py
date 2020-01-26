4b009fbe33663a43dadfeec3b06d14a716ab2706
Bobak Baghi <bobak.baghi@gmail.com>
2020-01-25 21:24:29 -0500

    Removed train function from policy classes.

--- neural_nets/base_network.py
+++ neural_nets/base_network.py
@@ -121,21 +121,6 @@ class BasePolicy(BaseNN):
 
         return self.sample_action(state)
 
-    def train(self, num_episodes, max_episode_length, reward_network=None):
-        """Train RL algorithm for num_episode number of episodes, and don't
-        exceed max_episode_length when sampling from the environment.
-        :param num_episodes: number of episodes to train RL for.
-        :type num_episodes: int
-        :param max_episode_length: maximum length of trajectories sampled
-        from environment (if any.)
-        :type max_episode_length: int
-        :param reward_network: A reward network for obtaining rewards,
-        defaults to None
-        :type reward_network: pytorch policy, optional
-        :return policy: trained policy.
-        """
-        raise NotImplementedError
-
 
 ModelTuple = namedtuple("ModelTuple", "model optimizer")
 
