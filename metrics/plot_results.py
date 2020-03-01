0993ca7b288ee655ee818dc730e68d65abd0016b
ranok92 <abhisek.konar@mail.mcgill.ca>
2020-02-29 23:48:12 -0500

    Added comments to the plotting script.

--- metrics/plot_results.py
+++ metrics/plot_results.py
@@ -13,8 +13,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--parent-directory', type=str, help="Name of the \
     parent directory containing the metric dictionaries.")
 
-parser.add_argument('--agent-names', nargs="*", help='List of agent names.')
-
 parser.add_argument('--metric-name', type=str, help='Name of the metric to be used for the \
                      plot.')
 
@@ -26,7 +24,7 @@ parser.add_argument('--fig-title', type=str, required=True)
 
 parser.add_argument('--ped-file-list', nargs="*", type=str,
                     default=["./ped_lists/easy.npy", 
-                    "./ped_lists/med.npy", "./ped_lists/hard.npy"] ,
+                    "./ped_lists/med.npy", "./ped_lists/hard.npy"],
                     help='List of the files containing the pedestrian ids to be \
                         used in the current metric plot.')
 
@@ -38,7 +36,6 @@ def read_data(list_of_files, ped_file_list):
     Reads list of data files and pedlist files
     """
     master_dictionary_list = []
-
     for agent_file in list_of_files:
         agent_dictionary_list = []
         for seed_file in agent_file:
@@ -60,7 +57,8 @@ def read_data(list_of_files, ped_file_list):
 
 
 def plot_histogram(list_of_dictionary, list_of_agent_names, 
-                   metric_name, fig_title, metric_info=None, ped_list=None):
+                   metric_name, fig_title, metric_info=None, 
+                   ped_list=None):
     """
     given a list of dictionary containing run information of different agents
     will return a histogram plot featuring the agent groups and the metrics provided
@@ -89,9 +87,10 @@ def plot_histogram(list_of_dictionary, list_of_agent_names,
     else:
         metric_value_len = len(metric_info)
 
+
     run_information_array = np.zeros([len(list_of_dictionary),
-                                     len(list_of_dictionary[0])*len(ped_list)],
-                                     metric_value_len)
+                                     len(list_of_dictionary[0])*len(ped_list),
+                                     metric_value_len])
 
     
     total_peds = len(ped_list)
@@ -136,17 +135,31 @@ def barplots_with_errorbars(list_of_dictionary, list_of_agent_names,
                               [dict_1_agent_1, dict_2_agent_1, . . ]
                               [dict_1_agent_2, dict_2_agent_2, . . ]
                              ] 
-        list_of_agent_names : a list of strings containing the name of the agents
-        metrics : a string containing the name of the metrics 
+        list_of_agent_names : a list of strings containing the name of the agents.
+                              This will be used to populate the legend.
+
+        metrics : a string containing the name of the metric
                   that needs to be retrieved from the dictionary.
                   Names should exactly match the key in the dictonary
-        fig_title : Title to be used in the figure.
+
+        fig_title : Title to be used in the figure. If metric_info is not none,
+                    this gets appended with the metric_info to create the final 
+                    title.
+
+        metric_info : If the information for the metric is of size>1, 
+                For example: 'compute_trajectory_smoothness' comprises of a 
+                    tuple (total change in orientation, 
+                            avg change in orientation), 
+                    then the metric_info
+                    should be a list of length 2 containing the description of the metric
+                    eg: 'total smoothness' 'avg smoothness'
+
         ped_list : a sorted numpy array containing the id of the 
                   pedestrians for whom the metric information 
                   needs to be retrieved
 
     output:
-        a barplot with error bars for the agents and metric
+        a barplot with error bars against each value in the metric
 
     """
 
@@ -167,14 +180,12 @@ def barplots_with_errorbars(list_of_dictionary, list_of_agent_names,
             #reading data from a single metric dictionary
             i = 0
             for ped in ped_list:
-                print(ped)
                 run_information_array[agent_counter]\
                         [(seed_counter*total_peds)+i][:] = run_info['metric_results'][ped]\
                                                                 [metric_name][0]
                 i += 1
             seed_counter+=1
         agent_counter += 1
-    pdb.set_trace()
 
     #plotting parameters
     bins = 200
@@ -201,12 +212,16 @@ def barplots_with_errorbars(list_of_dictionary, list_of_agent_names,
         title = fig_title+metric_info[info]
         ax.set_title(title)
         plt.show()
-    
 
+        file_name = title+'.fig.pickle'
+        file_name_eps = title+'.svg'
+        with open(file_name, 'w') as f:
+            pickle.dump(fig, f)
+        plt.savefig(file_name_eps)
 
 def plot_information_per_time_frame(list_of_dictionary, list_of_agent_names, 
                                     metric_name, fig_title, metric_info=None,
-                                    ped_list=None, max_traj_len=200):
+                                    ped_list=None):
     """
     Generates a plot of the metric against time for the given pedestrians 
     in the pedestrian list
@@ -216,49 +231,60 @@ def plot_information_per_time_frame(list_of_dictionary, list_of_agent_names,
                               [dict_1_agent_1, dict_2_agent_1, . . ]
                               [dict_1_agent_2, dict_2_agent_2, . . ]
                              ] 
-        list_of_agent_names : a list of strings containing the name of the agents
-        metrics : a string containing the name of the metrics
+        list_of_agent_names : a list of strings containing the name of the agents.
+                              This will be used to populate the legend.
+
+        metrics : a string containing the name of the metric
                   that needs to be retrieved from the dictionary.
                   Names should exactly match the key in the dictonary
-        fig_title : Title to be used in the figure.
-        metric_info : If the information is of size>1, what do they mean:
-                        eg. 'compute_trajectory_smoothness' is a tuple of
-                        size 2 containing the
-                        (total change in orientation, avg change in orientation)
+
+        fig_title : Title to be used in the figure. If metric_info is not none,
+                    this gets appended with the metric_info to create the final 
+                    title.
+        metric_info : If the information for the metric is of size>1, 
+                        For example: 'compute_trajectory_smoothness' comprises of a 
+                            tuple (total change in orientation, 
+                                    avg change in orientation), 
+                            then the metric_info
+                            should be a list of length 2 containing the description of the metric
+                            eg: 'total smoothness' 'avg smoothness'
+                        
         ped_list : a sorted numpy array containing the id of the
                   pedestrians for whom the metric information
-                  needs to be retrieved
+                  needs to be retrieved.
+                  **N.B. This method will produce a separate plot for results 
+                  against each pedestrian to prevent cluttering.
 
     output:
-        a barplot with error bars for the agents and metric
-
+        a line plot with errorbars where the xaxis is the time frame and y axis is 
+        the value of the metric
     """
 
     if metric_info is None:
         metric_value_len = 1
     else:
         metric_value_len = len(metric_info)
-    run_information_array = np.zeros([len(list_of_dictionary),
-                                     len(list_of_dictionary[0])*len(ped_list),
-                                     metric_value_len])
 
     total_agents = len(list_of_agent_names)
     total_peds = len(ped_list)
-    fig, ax = plt.subplots()
-
+    
+    max_traj_len = -1
+    for ped in ped_list:
+        for agent in list_of_dictionary:
+            for run_info in agent:
+                if len(run_info['metric_results'][ped][metric_name][0]) > max_traj_len:
+                    max_traj_len = len(run_info['metric_results'][ped][metric_name][0])
     x_axis = np.arange(max_traj_len)
     for ped in ped_list:
         agent_counter = 0
-
+        fig, ax = plt.subplots()
         for agent in list_of_dictionary:
             #reading data from a single metric dictionary
-            indiv_ped_traj_info_list = []
-            agent_counter = 0
-            mask_array = np.ma.empty((total_agents, max_traj_len))
+            total_seeds = len(agent)
+            mask_array = np.ma.empty((total_seeds, max_traj_len))
             mask_array.mask = True
             seed_counter = 0
             for run_info in agent:
-                print(ped)
                 cur_traj_len = len(run_info['metric_results'][ped][metric_name][0])
                 mask_array[seed_counter, 0:cur_traj_len] = (run_info['metric_results'][ped]\
                                                                 [metric_name][0])
@@ -267,47 +293,77 @@ def plot_information_per_time_frame(list_of_dictionary, list_of_agent_names,
             mean_arr = mask_array.mean(axis=0)
             std_arr = mask_array.std(axis=0)
 
-            ax.plot(mean_arr, label=list_of_agent_names[agent_counter])
-            ax.fill_between(x_axis, mean_arr-std_arr, mean_arr+std_arr)
+            ax.plot(mean_arr, label=list_of_agent_names[agent_counter], alpha=0.8)
+            ax.fill_between(x_axis, mean_arr-std_arr, mean_arr+std_arr, alpha=0.3)
             ax.set_xticks(x_axis)
             ax.yaxis.grid(True)
+            ax.legend()
             title = fig_title
             ax.set_title(title)
-        agent_counter += 1
-    
-    plt.show()
-    pdb.set_trace()
+            agent_counter += 1
 
+        plt.show()
+
+    
 if __name__=='__main__':
 
     args = parser.parse_args()
 
 
     metric_info_dict = read_files_from_directories(args.parent_directory)
-    pdb.set_trace()
-    '''
-    dict_files = [
-                 ['./results/Fahad/Fahad_seed2_highest_score_2020-02-28-15:07'], 
-                 ['./results/DroneFeatureRisk_speedv2_Smoothing/Seed9/DroneFeature_smooth_seed9_highest_score_2020-02-28-15:21']
-                 ]
-    '''
+    agent_names = []
+    file_list = []
+    for key in metric_info_dict.keys():
 
-    dict_files = [
-        ['./results/GoalConditionedFahad/GoalConditionedFahad_seed96_highest_score_2020-02-29-07:12']
-                 ]
-    master_dictionary_list, ped_list = read_data(dict_files, args.ped_file_list)
-    '''
-    plot_histogram(master_dictionary_list, args.agent_names, 
-                   args.metric_name, args.fig_title, ped_list)
+        if len(metric_info_dict[key]) > 0:
+            agent_names.append(key)
+            file_list.append(metric_info_dict[key])
+
+
+    master_dictionary_list, ped_list = read_data(file_list, args.ped_file_list)
+    #################################################
+    #uncomment this function to get historgram plots.
+    """
+    Sample commandline command:
+        python plot_results.py --parent-directory './results' 
+                               --fig-title 'Distance displacement ratio'
+                               --metric-name 'compute_distance_displacement_ratio'
+
+    """
+    plot_histogram(master_dictionary_list, agent_names, 
+                   args.metric_name, args.fig_title, ped_list=ped_list)
    
-
-    barplots_with_errorbars(master_dictionary_list, args.agent_names, 
+    #################################################
+    #uncomment this to get barplots with erros
+    """
+    Sample commandline command:
+         python plot_results.py --parent-directory './results'
+                                --fig-title 'Trajectory smoothness-'
+                                --metric-name 'compute_trajectory_smoothness'
+                                --metric-info 'total' 'average'
+    
+    barplots_with_errorbars(master_dictionary_list, agent_names, 
                    args.metric_name, args.fig_title,
                    metric_info=args.metric_info,
                    ped_list=ped_list) 
-    '''
+    
+    """
+    
+    #################################################
+    #uncomment this to get line plots over time frames.
+    """
+    Sample commandline command:
+        python plot_results.py --parent-directory './results' 
+                               --fig-title 'Distance_to_nearest_ped_over_time'  
+                               --metric-name 'distance_to_nearest_pedestrian_over_time'
+        
+        **here the ped_list is modified below.
+
+    
+    ped_list = [11, 12, 13]
     plot_information_per_time_frame(master_dictionary_list, 
-                                    args.agent_names, 
+                                    agent_names, 
                                     args.metric_name, args.fig_title,
                                     metric_info=args.metric_info,
-                                    ped_list=[1]) 
+                                    ped_list=ped_list) 
+    """
