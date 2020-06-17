399126f456b47b9ef21944fe084774aa6d4188e7
Bobak Baghi <bobak.baghi@gmail.com>
2020-06-17 15:58:03 -0400

    Added tests for speed_features.

--- featureExtractor/test_drone_feature_extractor.py
+++ featureExtractor/test_drone_feature_extractor.py
@@ -5,7 +5,7 @@ import numpy as np
 from numpy import testing as nptest
 from hypothesis import strategies as st
 from hypothesis.extra import numpy as npthesis
-from hypothesis import given, settings
+from hypothesis import given, settings, assume
 
 sys.path.insert(0, "..")
 
@@ -27,6 +27,18 @@ def is_onehot(arr):
     zeros = np.isclose(arr, np.zeros(arr.shape))
     ones = np.isclose(arr, np.ones(arr.shape))
     return np.all(np.logical_or(zeros, ones))
+
+
+def is_magnitude(arr):
+    """
+    Checks if array is an integer magnitude array.
+
+    :param arr: array to test.
+    :type arr: np.array
+    :return: True if array is a magnitude array.
+    :rtype: Boolean.
+    """
+    return np.all(np.isclose(np.mod(arr, 1), np.zeros(arr.shape)))
 
 
 class VectorMathTests(unittest.TestCase):
@@ -156,10 +168,151 @@ class FeatureFunctionTests(unittest.TestCase):
         Tests if drone_feature_extractor.radial_density_features is one-hot
         encoded or not.
         """
-        # import pdb
-
-        # pdb.set_trace()
         radial_feature = dfe.radial_density_features(
             agent_pos, ped_positions, radius
         )
         self.assertTrue(is_onehot(radial_feature))
+
+    @given(
+        npthesis.arrays(np.float, (2,)),
+        npthesis.arrays(
+            np.float,
+            st.tuples(st.integers(min_value=0, max_value=10000), st.just(2)),
+        ),
+        st.floats(),
+    )
+    def test_radial_density_length(self, agent_pos, ped_positions, radius):
+        """
+        Tests if drone_feature_extractor.radial_density_features is one-hot
+        encoded or not.
+        """
+        self.assertEqual(
+            len(dfe.radial_density_features(agent_pos, ped_positions, radius)),
+            3,
+        )
+
+    def test_speed_features(self):
+        """
+        Example testing for dfe.speed_features function.
+        """
+        low_thresh = 5.0
+        high_thresh = 100.00
+
+        nptest.assert_array_almost_equal(
+            dfe.speed_features(
+                (0.0, 1.0),
+                np.array([[0.0, 0.0], [10.0, 10.0], [1e6, 1e6]]),
+                lower_threshold=low_thresh,
+                upper_threshold=high_thresh,
+            ),
+            np.array([1.0, 1.0, 1.0]),
+        )
+        nptest.assert_array_almost_equal(
+            dfe.speed_features(
+                (0.0, 0.0),
+                np.array([[10.0, 10.0], [1e6, 1e6]]),
+                lower_threshold=low_thresh,
+                upper_threshold=high_thresh,
+            ),
+            np.array([0.0, 1.0, 1.0]),
+        )
+        nptest.assert_array_almost_equal(
+            dfe.speed_features((0.0, 0.0), np.empty((0, 2))),
+            np.array([0.0, 0.0, 0.0]),
+        )
+
+    @given(
+        npthesis.arrays(
+            np.float,
+            (2,),
+            elements=st.floats(allow_nan=False, allow_infinity=False),
+        ),
+        npthesis.arrays(
+            np.float,
+            st.tuples(st.integers(min_value=0, max_value=10000), st.just(2)),
+            elements=st.floats(allow_nan=False, allow_infinity=False),
+        ),
+        st.floats(),
+        st.floats(),
+    )
+    def test_speed_features_magnitude(
+        self, agent_vel, ped_vels, low_thresh, upper_thresh
+    ):
+        """
+        Tests whether speed_features returns a magnitude array or not.
+        """
+        assume(low_thresh < upper_thresh)
+        self.assertTrue(
+            is_magnitude(
+                dfe.speed_features(
+                    agent_vel,
+                    ped_vels,
+                    lower_threshold=low_thresh,
+                    upper_threshold=upper_thresh,
+                )
+            )
+        )
+
+    @given(
+        npthesis.arrays(
+            np.float,
+            (2,),
+            elements=st.floats(allow_nan=False, allow_infinity=False),
+        ),
+        npthesis.arrays(
+            np.float,
+            st.tuples(st.integers(min_value=0, max_value=10000), st.just(2)),
+            elements=st.floats(allow_nan=False, allow_infinity=False),
+        ),
+        st.floats(),
+        st.floats(),
+    )
+    def test_speed_features_all_accounted(
+        self, agent_vel, ped_vels, low_thresh, upper_thresh
+    ):
+        """
+        Tests whether speed_features returns a magnitude array or not.
+        """
+        assume(low_thresh < upper_thresh)
+        nptest.assert_almost_equal(
+            dfe.speed_features(
+                agent_vel,
+                ped_vels,
+                lower_threshold=low_thresh,
+                upper_threshold=upper_thresh,
+            ).sum(),
+            len(ped_vels),
+        )
+
+    @given(
+        npthesis.arrays(
+            np.float,
+            (2,),
+            elements=st.floats(allow_nan=False, allow_infinity=False),
+        ),
+        npthesis.arrays(
+            np.float,
+            st.tuples(st.integers(min_value=0, max_value=10000), st.just(2)),
+            elements=st.floats(allow_nan=False, allow_infinity=False),
+        ),
+        st.floats(),
+        st.floats(),
+    )
+    def test_speed_features_length(
+        self, agent_vel, ped_vels, low_thresh, upper_thresh
+    ):
+        """
+        Tests whether speed_features returns a magnitude array or not.
+        """
+        assume(low_thresh < upper_thresh)
+        self.assertEqual(
+            len(
+                dfe.speed_features(
+                    agent_vel,
+                    ped_vels,
+                    lower_threshold=low_thresh,
+                    upper_threshold=upper_thresh,
+                )
+            ),
+            3,
+        )
