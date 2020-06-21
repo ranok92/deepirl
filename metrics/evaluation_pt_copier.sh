453c527891cb6a60bc5f1e0aecfac0b7b52c3caf
Bobak Baghi <bobak.baghi@gmail.com>
2020-06-21 02:52:38 -0400

    added copying support for RL experiments.

--- metrics/evaluation_pt_copier.sh
+++ metrics/evaluation_pt_copier.sh
@@ -8,14 +8,23 @@ LOW_SVF_FINDER_DIR=low_svf_finder.py
 for res_type in $ALL_RES_PATH/*; do
     echo "found result type" $res_type
     for seed in $res_type/*; do
-       # low svf symlink
-       low_svf="$(python -W ignore $LOW_SVF_FINDER_DIR --tf-file $seed/tf_logs/* | sed -nr 's/.*step\s([0-9]+)/\1/p').pt"
-       echo "creating symlink $seed/$low_svf into $seed"
-       ln -s $seed/$low_svf $seed
+        # low svf symlink
+        if [ -d $seed/tf_logs/ ]; then
+            low_svf="$(python -W ignore $LOW_SVF_FINDER_DIR --tf-file $seed/tf_logs/* | sed -nr 's/.*step\s([0-9]+)/\1/p').pt"
+            echo "creating symlink $seed/$low_svf into $seed"
+            ln -sf $seed/$low_svf $seed
+        fi
 
-       # latest iteration symlink
-       last_iter="$(ls -v $seed/saved-models | tail -1)"
-       echo "creating symlink $seed/$last_iter into $seed"
-       ln -s $seed/$last_iter $seed
+        # latest iteration symlink
+        if [ -d $seed/saved-models/ ]; then
+            last_iter="$(ls -v $seed/saved-models | tail -1)"
+            echo "creating symlink $seed/$last_iter into $seed"
+            ln -sf $seed/$last_iter $seed
+        fi
+        if [ -d $seed/policy-models/ ]; then
+            last_iter="$(ls -v $seed/policy-models | tail -1)"
+            echo "creating symlink $seed/$last_iter into $seed"
+            ln -sf $seed/$last_iter $seed
+        fi
     done
 done
